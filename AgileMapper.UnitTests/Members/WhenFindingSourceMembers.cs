035b62a6a79b61eb711eb7ab1befdd892646e963
Steve Wilkes <steve@agileobjects.co.uk>
2016-04-29 12:06:51 -0700

    MemberFinder tests

---
+++ AgileMapper.UnitTests/Members/WhenFindingSourceMembers.cs
@@ -0,0 +1,120 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Members
+{
+    using System;
+    using System.Linq;
+    using AgileMapper.Members;
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    // ReSharper disable PossibleNullReferenceException
+    public class WhenFindingSourceMembers
+    {
+        private static readonly MemberFinder _memberFinder = new MemberFinder();
+
+        [Fact]
+        public void ShouldFindAPublicProperty()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(PublicProperty<string>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldNotBeNull();
+
+            member.Type.ShouldBe(typeof(string));
+        }
+
+        [Fact]
+        public void ShouldFindAPublicField()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(PublicField<int>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldNotBeNull();
+            member.Type.ShouldBe(typeof(int));
+        }
+
+        [Fact]
+        public void ShouldFindAPublicGetMethod()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(PublicGetMethod<DateTime>))
+                .FirstOrDefault(m => m.Name == "GetValue");
+
+            member.ShouldNotBeNull();
+            member.Type.ShouldBe(typeof(DateTime));
+        }
+
+        [Fact]
+        public void ShouldIgnoreAWriteOnlyPublicProperty()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(PublicWriteOnlyProperty<long>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldIgnoreANonPublicField()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(InternalField<byte>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldIgnoreASetMethod()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(PublicSetMethod<short>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldIgnoreGetType()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(PublicProperty<int?>))
+                .FirstOrDefault(m => m.Name == "GetType");
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldIgnoreGetHashCode()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(PublicProperty<DateTime?>))
+                .FirstOrDefault(m => m.Name == "GetHashCode");
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldIgnoreAPropertyGetter()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(PublicProperty<string>))
+                .FirstOrDefault(m => m.Name.StartsWith("get_"));
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldFindARootArrayElement()
+        {
+            var member = _memberFinder
+                .GetSourceMembers(typeof(int[]))
+                .FirstOrDefault();
+
+            member.ShouldNotBeNull();
+        }
+    }
+    // ReSharper restore PossibleNullReferenceException
+}
---
+++ AgileMapper.UnitTests/Members/WhenFindingTargetMembers.cs
@@ -0,0 +1,100 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Members
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using AgileMapper.Members;
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    // ReSharper disable PossibleNullReferenceException
+    public class WhenFindingTargetMembers
+    {
+        private static readonly MemberFinder _memberFinder = new MemberFinder();
+
+        [Fact]
+        public void ShouldFindAPublicProperty()
+        {
+            var member = _memberFinder
+                .GetTargetMembers(typeof(PublicProperty<byte>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldNotBeNull();
+            member.Type.ShouldBe(typeof(byte));
+        }
+
+        [Fact]
+        public void ShouldFindAPublicField()
+        {
+            var member = _memberFinder
+                .GetTargetMembers(typeof(PublicField<int>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldNotBeNull();
+            member.Type.ShouldBe(typeof(int));
+        }
+
+        [Fact]
+        public void ShouldFindAPublicSetMethod()
+        {
+            var member = _memberFinder
+                .GetTargetMembers(typeof(PublicSetMethod<DateTime>))
+                .FirstOrDefault(m => m.Name == "SetValue");
+
+            member.ShouldNotBeNull();
+            member.Type.ShouldBe(typeof(DateTime));
+        }
+
+        [Fact]
+        public void ShouldIgnoreAReadOnlyPublicProperty()
+        {
+            var member = _memberFinder
+                .GetTargetMembers(typeof(PublicReadOnlyProperty<long>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldIgnoreANonPublicField()
+        {
+            var member = _memberFinder
+                .GetTargetMembers(typeof(InternalField<List<byte>>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldIgnoreAReadOnlyField()
+        {
+            var member = _memberFinder
+                .GetTargetMembers(typeof(ReadOnlyField<IEnumerable<byte>>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldIgnoreAGetMethod()
+        {
+            var member = _memberFinder
+                .GetTargetMembers(typeof(PublicGetMethod<string[]>))
+                .FirstOrDefault(m => m.Name == "Value");
+
+            member.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldIgnoreAPropertySetter()
+        {
+            var member = _memberFinder
+                .GetTargetMembers(typeof(PublicProperty<long>))
+                .FirstOrDefault(m => m.Name.StartsWith("set_"));
+
+            member.ShouldBeNull();
+        }
+    }
+    // ReSharper restore PossibleNullReferenceException
+}
