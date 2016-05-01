09c00868766b82e3fd831a21ba4a24399467fb7f
Steve Wilkes <steve@agileobjects.co.uk>
2016-05-01 12:09:32 -0700

    Invalid configuration test

---
+++ AgileMapper.UnitTests/Configuration/WhenSettingInvalidConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Configuration
+{
+    using System;
+    using Api.Configuration;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenSettingInvalidConfiguration
+    {
+        [Fact]
+        public void ShouldErrorIfUnconvertibleConstantSpecified()
+        {
+            Assert.Throws<MappingConfigurationException>(() =>
+            {
+                using (var mapper = new Mapper())
+                {
+                    mapper.When.Mapping
+                        .From<PublicField<int>>()
+                        .To<PublicField<DateTime>>()
+                        .Map(new byte[] { 2, 4, 6, 8 })
+                        .To(x => x.Value);
+                }
+            });
+        }
+    }
+}
