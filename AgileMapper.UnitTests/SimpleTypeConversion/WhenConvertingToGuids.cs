21704ec59592df004957a73d2603ac90876dd727
Steve Wilkes <steve@agileobjects.co.uk>
2016-05-01 14:09:55 -0700

    -> Guid conversion test coverage

---
+++ AgileMapper.UnitTests/SimpleTypeConversion/WhenConvertingToGuids.cs
@@ -0,0 +1,38 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.SimpleTypeConversion
+{
+    using System;
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenConvertingToGuids
+    {
+        [Fact]
+        public void ShouldMapAStringToAGuid()
+        {
+            var guid = Guid.NewGuid();
+            var source = new PublicGetMethod<string>(guid.ToString());
+            var result = Mapper.Map(source).ToNew<PublicSetMethod<Guid>>();
+
+            result.Value.ShouldBe(guid);
+        }
+
+        [Fact]
+        public void ShouldMapAGuidToANullableGuid()
+        {
+            var source = new PublicGetMethod<Guid>(Guid.NewGuid());
+            var result = Mapper.Map(source).ToNew<PublicField<Guid?>>();
+
+            result.Value.ShouldBe(source.GetValue());
+        }
+
+        [Fact]
+        public void ShouldMapANullableGuidToAGuid()
+        {
+            var source = new PublicGetMethod<Guid?>(Guid.NewGuid());
+            var result = Mapper.Map(source).ToNew<PublicField<Guid>>();
+
+            result.Value.ShouldBe(source.GetValue().GetValueOrDefault());
+        }
+    }
+}
