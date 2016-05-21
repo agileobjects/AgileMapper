b879125356bcd513b303b9e6f9466a9188bc9493
Steve <steve@agileobjects.co.uk>
2016-05-21 16:48:29 -0700

    Support for derived source-target type pair configuration for enumerables

--- AgileMapper.UnitTests/Configuration/WhenConfiguringDerivedTypePairs.cs
+++ AgileMapper.UnitTests/Configuration/WhenConfiguringDerivedTypePairs.cs
@@ -1,5 +1,7 @@
 ﻿namespace AgileObjects.AgileMapper.UnitTests.Configuration
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using Shouldly;
     using TestClasses;
     using Xunit;
@@ -40,6 +42,25 @@
                 var result = mapper.Map(source).OnTo(target);
 
                 result.Value.ShouldBeOfType<CustomerViewModel>();
+            }
+        }
+
+        [Fact]
+        public void ShouldCreateARootEnumerableDerivedTargetElementFromADerivedSourceElement()
+        {
+            using (var mapper = Mapper.Create())
+            {
+                mapper.WhenMapping
+                    .From<PersonViewModel>()
+                    .To<Person>()
+                    .Map<CustomerViewModel>()
+                    .To<Customer>();
+
+                var source = new[] { new PersonViewModel(), new CustomerViewModel() };
+                var result = mapper.Map(source).ToNew<ICollection<Person>>();
+
+                result.First().ShouldBeOfType<Person>();
+                result.Second().ShouldBeOfType<Customer>();
             }
         }
     }
--- AgileMapper/Api/Configuration/DerivedTypePair.cs
+++ AgileMapper/Api/Configuration/DerivedTypePair.cs
@@ -1,9 +1,12 @@
 ﻿namespace AgileObjects.AgileMapper.Api.Configuration
 {
     using System;
+    using Members;
 
     internal class DerivedTypePair : UserConfiguredItemBase
     {
+        private readonly Type _derivedSourceType;
+
         public DerivedTypePair(
             MappingConfigInfo configInfo,
             Type mappingTargetType,
@@ -11,9 +14,13 @@
             Type derivedTargetType)
             : base(configInfo, mappingTargetType)
         {
+            _derivedSourceType = derivedSourceType;
             DerivedTargetType = derivedTargetType;
         }
 
         public Type DerivedTargetType { get; }
+
+        public override bool AppliesTo(IMappingData data)
+            => _derivedSourceType.IsAssignableFrom(data.SourceType) && base.AppliesTo(data);
     }
 }
