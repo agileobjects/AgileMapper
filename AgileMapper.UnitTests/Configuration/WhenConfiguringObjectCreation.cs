0c5ebace39b2e4db788b2a366a589a2a7ce2ded3
Steve <steve@agileobjects.co.uk>
2016-06-04 19:38:55 -0700

    Test coverage for multiple configured particular-type object factories

--- AgileMapper.UnitTests/Configuration/WhenConfiguringObjectCreation.cs
+++ AgileMapper.UnitTests/Configuration/WhenConfiguringObjectCreation.cs
@@ -1,6 +1,7 @@
 ﻿namespace AgileObjects.AgileMapper.UnitTests.Configuration
 {
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Linq;
     using Shouldly;
     using TestClasses;
@@ -71,8 +72,47 @@
                 var matchingSource = new CustomerViewModel { Discount = 10 };
                 var matchingResult = mapper.Map(matchingSource).ToNew<PublicField<List<int>>>();
 
-                matchingResult.Value.Count.ShouldBe(1);
+                matchingResult.Value.ShouldNotBeNull();
+                matchingResult.Value.ShouldHaveSingleItem();
                 matchingResult.Value.First().ShouldBe(10);
+            }
+        }
+
+        [Fact]
+        public void ShouldUseConfiguredFactoriesForASpecifiedSourceAndTargetTypeConditionally()
+        {
+            using (var mapper = Mapper.Create())
+            {
+                mapper.WhenMapping
+                    .From<PersonViewModel>()
+                    .To<PublicField<ICollection<string>>>()
+                    .CreateInstancesUsing(ctx => new PublicField<ICollection<string>>
+                    {
+                        Value = new Collection<string> { ctx.Source.Name }
+                    });
+
+                mapper.WhenMapping
+                    .From<CustomerViewModel>()
+                    .To<PublicField<ICollection<string>>>()
+                    .If((cvm, c) => cvm.Discount > 0)
+                    .CreateInstancesUsing(ctx => new PublicField<ICollection<string>>
+                    {
+                        Value = new[] { ctx.Source.Name + $" ({ctx.Source.Discount})" }
+                    });
+
+                var unconditionalFactorySource = new CustomerViewModel { Name = "Steve", Discount = 0 };
+                var unconditionalFactoryResult = mapper.Map(unconditionalFactorySource).ToNew<PublicField<ICollection<string>>>();
+
+                unconditionalFactoryResult.Value.ShouldNotBeNull();
+                unconditionalFactoryResult.Value.ShouldHaveSingleItem();
+                unconditionalFactoryResult.Value.First().ShouldBe("Steve");
+
+                var conditionalFactorySource = new CustomerViewModel { Name = "Alex", Discount = 1 };
+                var conditionalFactoryResult = mapper.Map(conditionalFactorySource).ToNew<PublicField<ICollection<string>>>();
+
+                conditionalFactoryResult.Value.ShouldNotBeNull();
+                conditionalFactoryResult.Value.ShouldHaveSingleItem();
+                conditionalFactoryResult.Value.First().ShouldBe("Alex (1)");
             }
         }
 
