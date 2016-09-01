9963ae09c973782fa588fec0a3106153b8403d54
Steve Wilkes <steve@agileobjects.co.uk>
2016-09-01 17:08:10 +0100

    Adding API documentation

--- AgileMapper/Api/PlanTargetTypeSelector.cs
+++ AgileMapper/Api/PlanTargetTypeSelector.cs
@@ -2,6 +2,11 @@
 {
     using Plans;
 
+    /// <summary>
+    /// Provides options to create and compile mapping functions for a particular type of mapping from the 
+    /// source type being configured to a specified target type.
+    /// </summary>
+    /// <typeparam name="TSource">The source type to which the configuration should apply.</typeparam>
     public class PlanTargetTypeSelector<TSource>
     {
         private readonly MapperContext _mapperContext;
@@ -11,12 +16,30 @@
             _mapperContext = mapperContext;
         }
 
+        /// <summary>
+        /// Create and compile mapping functions for a create new mapping from the source type being 
+        /// configured to the type specified by the type argument.
+        /// </summary>
+        /// <typeparam name="TResult">The type of object for which to create the mapping plans.</typeparam>
+        /// <returns>A string mapping plan showing the functions to be executed during a mapping.</returns>
         public string ToANew<TResult>() where TResult : class
             => GetMappingPlan<TResult>(_mapperContext.RuleSets.CreateNew);
 
+        /// <summary>
+        /// Create and compile mapping functions for an OnTo (merge) mapping from the source type being 
+        /// configured to the type specified by the type argument.
+        /// </summary>
+        /// <typeparam name="TTarget">The type of object for which to create the mapping plans.</typeparam>
+        /// <returns>A string mapping plan showing the functions to be executed during a mapping.</returns>
         public string OnTo<TTarget>() where TTarget : class
             => GetMappingPlan<TTarget>(_mapperContext.RuleSets.Merge);
 
+        /// <summary>
+        /// Create and compile mapping functions for an Over (overwrite) mapping from the source type being 
+        /// configured to the type specified by the type argument.
+        /// </summary>
+        /// <typeparam name="TTarget">The type of object for which to create the mapping plans.</typeparam>
+        /// <returns>A string mapping plan showing the functions to be executed during a mapping.</returns>
         public string Over<TTarget>() where TTarget : class
             => GetMappingPlan<TTarget>(_mapperContext.RuleSets.Overwrite);
 
--- AgileMapper/Api/TargetTypeSelector.cs
+++ AgileMapper/Api/TargetTypeSelector.cs
@@ -1,5 +1,9 @@
 ﻿namespace AgileObjects.AgileMapper.Api
 {
+    /// <summary>
+    /// Provides options to specify the type of mapping to perform.
+    /// </summary>
+    /// <typeparam name="TSource">The source type to which the configuration should apply.</typeparam>
     public class TargetTypeSelector<TSource>
     {
         private readonly TSource _source;
@@ -11,12 +15,29 @@
             _mapperContext = mapperContext;
         }
 
+        /// <summary>
+        /// Perform a new object mapping.
+        /// </summary>
+        /// <typeparam name="TResult">The type of object to create from the specified source object.</typeparam>
+        /// <returns>The result of the new object mapping.</returns>
         public TResult ToANew<TResult>() where TResult : class
             => PerformMapping(_mapperContext.RuleSets.CreateNew, default(TResult));
 
+        /// <summary>
+        /// Perform an OnTo (merge) mapping.
+        /// </summary>
+        /// <typeparam name="TTarget">The type of object on which to perform the mapping.</typeparam>
+        /// <param name="existing">The object on which to perform the mapping.</param>
+        /// <returns>The mapped object.</returns>
         public TTarget OnTo<TTarget>(TTarget existing) where TTarget : class
             => PerformMapping(_mapperContext.RuleSets.Merge, existing);
 
+        /// <summary>
+        /// Perform an Over (overwrite) mapping.
+        /// </summary>
+        /// <typeparam name="TTarget">The type of object on which to perform the mapping.</typeparam>
+        /// <param name="existing">The object on which to perform the mapping.</param>
+        /// <returns>The mapped object.</returns>
         public TTarget Over<TTarget>(TTarget existing) where TTarget : class
             => PerformMapping(_mapperContext.RuleSets.Overwrite, existing);
 
