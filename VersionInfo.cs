e3decac3e354e797a5857e548ab29f3551b1bd96
Steve <steve@agileobjects.co.uk>
2016-11-12 14:40:32 +0000

    Updating to v0.6

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.0")]
-[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyVersion("0.6.0")]
+[assembly: AssemblyFileVersion("0.6.0")]
