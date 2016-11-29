e0958772b6b308ec80bcfd93051a81204544bc87
Steve <steve@agileobjects.co.uk>
2016-11-29 13:45:57 +0000

    Adding null-checking and empty collection creation to ValueInjector deep and complex mappers

--- AgileMapper.PerformanceTester/ConcreteMappers/ValueInjecter/ValueInjecterDeepMapper.cs
+++ AgileMapper.PerformanceTester/ConcreteMappers/ValueInjecter/ValueInjecterDeepMapper.cs
@@ -1,5 +1,6 @@
 ﻿namespace AgileObjects.AgileMapper.PerformanceTester.ConcreteMappers.ValueInjecter
 {
+    using System.Collections.Generic;
     using System.Linq;
     using AbstractMappers;
     using Omu.ValueInjecter;
@@ -16,8 +17,14 @@
         {
             var dto = (CustomerDto)new CustomerDto().InjectFrom<FlatLoopInjection>(customer);
             dto.HomeAddress = (AddressDto)new AddressDto().InjectFrom(customer.HomeAddress);
-            dto.Addresses = customer.Addresses.Select(a => (AddressDto)new AddressDto().InjectFrom(a)).ToList();
-            dto.AddressesArray = customer.AddressesArray.Select(a => (AddressDto)new AddressDto().InjectFrom(a)).ToArray();
+
+            dto.Addresses =
+                customer.Addresses?.Select(a => (AddressDto)new AddressDto().InjectFrom(a)).ToList()
+                ?? new List<AddressDto>();
+
+            dto.AddressesArray =
+                customer.AddressesArray?.Select(a => (AddressDto)new AddressDto().InjectFrom(a)).ToArray()
+                ?? new AddressDto[0];
 
             return dto;
         }
