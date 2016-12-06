111ae9a778b6ccfa40fbe9c82935c2d7e1b4dee3
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2016-12-06 16:55:36 +0200

    Add AllowPartiallyTrustedCallers
    AllowPartiallyTrustedCallers skips some security checks in the CLR.
    http://stackoverflow.com/questions/5053032/performance-of-compiled-to-delegate-expression/5160513

--- AgileMapper.PerformanceTester/Properties/AssemblyInfo.cs
+++ AgileMapper.PerformanceTester/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
 [assembly: AssemblyTitle("AgileMapper.PerformanceTester")]
-
+[assembly: AllowPartiallyTrustedCallers]
 [assembly: ComVisible(false)]
