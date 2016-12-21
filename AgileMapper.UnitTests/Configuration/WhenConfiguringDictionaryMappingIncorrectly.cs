aa74d66d80733421297b98d42cd2bf073fed56bd
Steve <steve@agileobjects.co.uk>
2016-12-21 11:23:57 +0000

    Erroring if a null custom dictionary mapping key is supplied

--- AgileMapper.UnitTests/Configuration/WhenConfiguringDictionaryMappingIncorrectly.cs
+++ AgileMapper.UnitTests/Configuration/WhenConfiguringDictionaryMappingIncorrectly.cs
@@ -8,6 +8,21 @@
     public class WhenConfiguringDictionaryMappingIncorrectly
     {
         [Fact]
+        public void ShouldErrorIfCustomMemberKeyIsNull()
+        {
+            var configEx = Should.Throw<MappingConfigurationException>(() =>
+            {
+                Mapper.WhenMapping
+                    .FromDictionaries()
+                    .To<PublicField<string>>()
+                    .MapKey(null)
+                    .To(pf => pf.Value);
+            });
+
+            configEx.Message.ShouldContain("cannot be null");
+        }
+
+        [Fact]
         public void ShouldErrorIfCustomMemberNameIsNull()
         {
             var configEx = Should.Throw<MappingConfigurationException>(() =>
