f3ce6497808d9ae7f541ac9b7af6c6d4b38acbc4
Steve <steve@agileobjects.co.uk>
2017-01-18 08:30:44 +0000

    First target dictionary Over mapping tests

---
+++ AgileMapper.UnitTests/WhenMappingOverDictionaries.cs
@@ -0,0 +1,35 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests
+{
+    using System.Collections.Generic;
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenMappingOverDictionaries
+    {
+        [Fact]
+        public void ShouldOverwriteASimpleTypedDictionary()
+        {
+            var source = new Address { Line1 = "Here", Line2 = "There" };
+            var target = new Dictionary<string, string> { ["Line1"] = "La la la" };
+            var result = Mapper.Map(source).Over(target);
+
+            result.ShouldBeSameAs(target);
+            result["Line1"].ShouldBe("Here");
+            result["Line2"].ShouldBe("There");
+        }
+
+        [Fact]
+        public void ShouldNotRemoveUnmatchedEntries()
+        {
+            var source = new Address { Line1 = "Here", Line2 = null };
+            var target = new Dictionary<string, object> { ["Line3"] = "La la la" };
+            var result = Mapper.Map(source).Over(target);
+
+            result.ShouldBeSameAs(target);
+            result["Line1"].ShouldBe("Here");
+            result["Line2"].ShouldBeNull();
+            result["Line3"].ShouldBe("La la la");
+        }
+    }
+}
