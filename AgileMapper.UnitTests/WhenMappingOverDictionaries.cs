ff21dff66a0ad326698cee254f8fca25ef966297
Steve <steve@agileobjects.co.uk>
2017-01-19 09:12:50 +0000

    Test coverage for mapping a complex type collection over a dictionary

--- AgileMapper.UnitTests/WhenMappingOverDictionaries.cs
+++ AgileMapper.UnitTests/WhenMappingOverDictionaries.cs
@@ -1,6 +1,7 @@
 ﻿namespace AgileObjects.AgileMapper.UnitTests
 {
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using Shouldly;
     using TestClasses;
     using Xunit;
@@ -47,6 +48,29 @@
             result["[1]"].ShouldBe("2");
             result["[2]"].ShouldBe("3");
             result["[4]"].ShouldBe("6");
+        }
+
+        [Fact]
+        public void ShouldOverwriteAComplexTypeCollectionToADictionary()
+        {
+            var source = new Collection<Address>
+            {
+                new Address { Line1 = "1.1", Line2 = "1.2"},
+                new Address { Line1 = "2.1", Line2 = null }
+            };
+            var target = new Dictionary<string, object>
+            {
+                ["[0].Line1"] = "Old 1.1",
+                ["[0].Line2"] = "Old 1.2",
+                ["[1].Line1"] = "Old 2.1",
+                ["[1].Line2"] = "Old 2.2"
+            };
+            var result = Mapper.Map(source).Over(target);
+
+            result["[0].Line1"].ShouldBe("1.1");
+            result["[0].Line2"].ShouldBe("1.2");
+            result["[1].Line1"].ShouldBe("2.1");
+            result["[1].Line2"].ShouldBeNull();
         }
     }
 }
