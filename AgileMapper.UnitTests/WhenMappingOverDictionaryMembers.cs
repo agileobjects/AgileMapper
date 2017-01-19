01d27ea5f99054bec01ac71f67c7817a1f562538
Steve <steve@agileobjects.co.uk>
2017-01-19 11:22:03 +0000

    Start of nested dictionary map over tests

---
+++ AgileMapper.UnitTests/WhenMappingOverDictionaryMembers.cs
@@ -0,0 +1,26 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests
+{
+    using System.Collections.Generic;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenMappingOverDictionaryMembers
+    {
+        [Fact]
+        public void ShouldOverwriteANestedSimpleTypedIDictionary()
+        {
+            var source = new PublicField<Address>
+            {
+                Value = new Address { Line1 = "Here", Line2 = "There" }
+            };
+            var target = new PublicProperty<IDictionary<string, string>>
+            {
+                Value = new Dictionary<string, string> { ["Line1"] = "La la la" }
+            };
+            var result = Mapper.Map(source).Over(target);
+
+            result.Value["Line1"].ShouldBe("Here");
+            result.Value["Line2"].ShouldBe("There");
+        }
+    }
+}
