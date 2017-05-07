6c86b27895c23616693031bcb8423e8190ad968f
Steve Wilkes <steve@agileobjects.co.uk>
2017-05-07 21:17:21 +0100

    Updating main assembly description

--- AgileMapper/Properties/AssemblyInfo.cs
+++ AgileMapper/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Security;
 
 [assembly: AssemblyTitle("AgileObjects.AgileMapper")]
-[assembly: AssemblyDescription("AgileObjects.AgileMapper")]
+[assembly: AssemblyDescription("AgileMapper is a zero-configuration, highly-configurable object-object mapper with viewable execution plans via a static or instance API. It conforms to .NET Standard 1.0.")]
 [assembly: AllowPartiallyTrustedCallers]
 
 [assembly: InternalsVisibleTo("AgileObjects.AgileMapper.UnitTests.Polyfills, PublicKey=0024000004800000940000000602000000240000525341310004000001000100570b21a39fbc3df774a5e60b41cd41078ebae1c9210a3ae4355d518a0abecab27f9346fbfe941618dc835e99ab21b75ff38e5815dceebdd8480dc14c0ee14f5cdcd3ace7f980173238c9d827f95a6f46100ff19a7dcf912c9291bf95dcd64694692a193428f2a35023bbed186f3c8f9769e01e5077a8ea5cabafe5d7948024af")]
