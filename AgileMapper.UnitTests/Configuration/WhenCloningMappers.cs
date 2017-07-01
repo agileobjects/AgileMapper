4399dd3674f603fbfac8eb5e847891ba7d261d19
Steve Wilkes <steve@agileobjects.co.uk>
2017-07-01 17:57:41 +0100

    Test coverage for conditionally-ignoring a target member in a cloned mapper when the original mapper unconditionally ignores the same member

--- AgileMapper.UnitTests/Configuration/WhenCloningMappers.cs
+++ AgileMapper.UnitTests/Configuration/WhenCloningMappers.cs
@@ -181,6 +181,46 @@
         }
 
         [Fact]
+        public void ShouldSupportConditionallyIgnoringAParentIgnoredMember()
+        {
+            using (var originalMapper = Mapper.CreateNew())
+            {
+                originalMapper.WhenMapping
+                    .From<Address>()
+                    .ToANew<Address>()
+                    .Ignore(ta => ta.Line2);
+
+                using (var clonedMapper = originalMapper.CloneSelf())
+                {
+                    clonedMapper.WhenMapping
+                        .From<Address>()
+                        .ToANew<Address>()
+                        .If(ctx => ctx.Source.Line1 == null)
+                        .Ignore(ta => ta.Line2);
+
+                    var source = new Address { Line1 = "There there", Line2 = "Here here" };
+
+                    var originalResult = originalMapper.Map(source).ToANew<Address>();
+
+                    originalResult.Line1.ShouldBe("There there");
+                    originalResult.Line2.ShouldBeNull();
+
+                    var clonedResult = clonedMapper.Map(source).ToANew<Address>();
+
+                    clonedResult.Line1.ShouldBe("There there");
+                    clonedResult.Line2.ShouldBeNull();
+
+                    source.Line1 = null;
+
+                    var nullLine1ClonedResult = clonedMapper.Map(source).ToANew<Address>();
+
+                    nullLine1ClonedResult.Line1.ShouldBeNull();
+                    nullLine1ClonedResult.Line2.ShouldBeNull("Here here");
+                }
+            }
+        }
+
+        [Fact]
         public void ShouldErrorIfClonedMapperHasConflictingDataSourceConfigured()
         {
             Should.Throw<MappingConfigurationException>(() =>
@@ -219,7 +259,7 @@
         }
 
         [Fact]
-        public void ShouldErrorIfRedundantMemberIgnoreIsConfigured()
+        public void ShouldErrorIfRedundantIgnoredMemberIsConfigured()
         {
             var ignoreEx = Should.Throw<MappingConfigurationException>(() =>
             {
