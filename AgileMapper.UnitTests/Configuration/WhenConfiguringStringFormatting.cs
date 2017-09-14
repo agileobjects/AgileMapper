a9b7b7eb693ff3c8f8a6ff0b29d01dcfd304c7e3
Steve Wilkes <steve@agileobjects.co.uk>
2017-09-14 11:19:34 +0100

    Test coverage for configured, mapper-wide decimal and double ToString formatting

--- AgileMapper.UnitTests/Configuration/WhenConfiguringStringFormatting.cs
+++ AgileMapper.UnitTests/Configuration/WhenConfiguringStringFormatting.cs
@@ -8,7 +8,7 @@
     {
         // See https://github.com/agileobjects/AgileMapper/issues/23
         [Fact]
-        public void ShouldFormatDateTimesGlobally()
+        public void ShouldFormatDateTimesMapperWide()
         {
             using (var mapper = Mapper.CreateNew())
             {
@@ -19,6 +19,36 @@
                 var result = mapper.Map(source).ToANew<PublicField<string>>();
 
                 result.Value.ShouldBe(source.Value.ToString("o"));
+            }
+        }
+
+        [Fact]
+        public void ShouldFormatDecimalsMapperWide()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                mapper.WhenMapping
+                    .StringsFrom<decimal>(c => c.FormatUsing("C"));
+
+                var source = new PublicProperty<decimal> { Value = 1.99m };
+                var result = mapper.Map(source).ToANew<PublicField<string>>();
+
+                result.Value.ShouldBe(source.Value.ToString("C"));
+            }
+        }
+
+        [Fact]
+        public void ShouldFormatDoublesMapperWideUsingDecimalPlaces()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                mapper.WhenMapping
+                    .StringsFrom<double>(c => c.FormatUsing("0.00"));
+
+                var source = new PublicProperty<double> { Value = 1 };
+                var result = mapper.Map(source).ToANew<PublicField<string>>();
+
+                result.Value.ShouldBe("1.00");
             }
         }
     }
