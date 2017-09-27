761e478da8df2d2751bf4fa42bcc07dab85406ef
Steve Wilkes <steve@agileobjects.co.uk>
2017-09-27 11:59:32 +0100

    Moving class to a dedicated file

--- AgileMapper/ObjectPopulation/DerivedComplexTypeMappingsFactory.cs
+++ AgileMapper/ObjectPopulation/DerivedComplexTypeMappingsFactory.cs
@@ -386,32 +386,4 @@ namespace AgileObjects.AgileMapper.ObjectPopulation
             public IList<DerivedTypePair> TypePairs { get; }
         }
     }
-
-    internal class DerivedSourceTypeCheck
-    {
-        private readonly Type _derivedSourceType;
-
-        public DerivedSourceTypeCheck(Type derivedSourceType)
-        {
-            _derivedSourceType = derivedSourceType;
-
-            var typedVariableName = "source" + derivedSourceType.GetVariableNameInPascalCase();
-            TypedVariable = Expression.Variable(derivedSourceType, typedVariableName);
-        }
-
-        public ParameterExpression TypedVariable { get; }
-
-        public Expression GetTypedVariableAssignment(IMemberMapperData declaredTypeMapperData)
-            => GetTypedVariableAssignment(declaredTypeMapperData.SourceObject);
-
-        public Expression GetTypedVariableAssignment(Expression sourceObject)
-        {
-            var typeAsConversion = Expression.TypeAs(sourceObject, _derivedSourceType);
-            var typedVariableAssignment = TypedVariable.AssignTo(typeAsConversion);
-
-            return typedVariableAssignment;
-        }
-
-        public Expression TypeCheck => TypedVariable.GetIsNotDefaultComparison();
-    }
 }
---
+++ AgileMapper/ObjectPopulation/DerivedSourceTypeCheck.cs
@@ -0,0 +1,35 @@
+namespace AgileObjects.AgileMapper.ObjectPopulation
+{
+    using System;
+    using System.Linq.Expressions;
+    using Extensions;
+    using Members;
+
+    internal class DerivedSourceTypeCheck
+    {
+        private readonly Type _derivedSourceType;
+
+        public DerivedSourceTypeCheck(Type derivedSourceType)
+        {
+            _derivedSourceType = derivedSourceType;
+
+            var typedVariableName = "source" + derivedSourceType.GetVariableNameInPascalCase();
+            TypedVariable = Expression.Variable(derivedSourceType, typedVariableName);
+        }
+
+        public ParameterExpression TypedVariable { get; }
+
+        public Expression GetTypedVariableAssignment(IMemberMapperData declaredTypeMapperData)
+            => GetTypedVariableAssignment(declaredTypeMapperData.SourceObject);
+
+        public Expression GetTypedVariableAssignment(Expression sourceObject)
+        {
+            var typeAsConversion = Expression.TypeAs(sourceObject, _derivedSourceType);
+            var typedVariableAssignment = TypedVariable.AssignTo(typeAsConversion);
+
+            return typedVariableAssignment;
+        }
+
+        public Expression TypeCheck => TypedVariable.GetIsNotDefaultComparison();
+    }
+}
