2c411cd8cb019a445580457a9019eb76e8e2c50a
Steve Wilkes <steve@agileobjects.co.uk>
2017-10-09 18:54:19 +0100

    Test coverage for mapping over struct members with default values

--- AgileMapper.UnitTests/Structs/WhenMappingOverStructs.cs
+++ AgileMapper.UnitTests/Structs/WhenMappingOverStructs.cs
@@ -22,5 +22,20 @@
             result.Value1.ShouldBe(source.Value1);
             result.Value2.ShouldBe(source.Value2);
         }
+
+        [Fact]
+        public void ShouldSetAnExistingSimpleTypePropertyValueToDefault()
+        {
+            var source = new PublicTwoFields<double?, int>();
+            var target = new PublicTwoFieldsStruct<double?, int> { Value1 = 537.0, Value2 = 6382 };
+
+            var result = Mapper.Map(source).Over(target);
+
+            target.Value1.ShouldBe(537.0m);
+            target.Value2.ShouldBe(6382);
+
+            result.Value1.ShouldBeNull();
+            result.Value2.ShouldBeDefault();
+        }
     }
 }
