8f0063103a0a93bfd0c0599c00f2331587d0bec9
Steve Wilkes <steve@agileobjects.co.uk>
2017-10-09 18:59:54 +0100

    Test coverage for overwriting a struct member to the default value

---
+++ AgileMapper.UnitTests/Structs/WhenMappingOverStructMembers.cs
@@ -0,0 +1,24 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Structs
+{
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenMappingOverStructMembers
+    {
+        [Fact]
+        public void ShouldOverwriteAMemberToDefault()
+        {
+            var source = new PublicProperty<PublicField<string>> { Value = null };
+            var target = new PublicProperty<PublicPropertyStruct<string>>
+            {
+                Value = new PublicPropertyStruct<string> { Value = "Gone" }
+            };
+
+            var result = Mapper.Map(source).Over(target);
+
+            result.ShouldBeSameAs(target);
+            result.Value.ShouldBeDefault();
+        }
+    }
+}
