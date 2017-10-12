4f53af90aa68fad9551137431c46d8cca9cf7b23
Steve Wilkes <steve@agileobjects.co.uk>
2017-10-12 15:26:43 +0100

    Updating to v0.14

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.13.0")]
-[assembly: AssemblyFileVersion("0.13.0")]
+[assembly: AssemblyVersion("0.14.0")]
+[assembly: AssemblyFileVersion("0.14.0")]
