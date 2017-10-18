5c8be09c1892ed16204fe78d0d3fe0682a6d6d13
Steve Wilkes <steve@agileobjects.co.uk>
2017-10-18 08:59:24 +0100

    Support for retrieving an existing inline mapper based on an inline configuration

--- AgileMapper.UnitTests/Configuration/Inline/WhenConfiguringMappingInline.cs
+++ AgileMapper.UnitTests/Configuration/Inline/WhenConfiguringMappingInline.cs
@@ -8,15 +8,24 @@
         [Fact]
         public void ShouldAllowInlineConstantDataSourceConfigurationViaStaticApi()
         {
-            var source = new PublicProperty<int> { Value = 1 };
+            var source1 = new PublicProperty<int> { Value = 1 };
+            var source2 = new PublicProperty<int> { Value = 2 };
 
-            var result = Mapper
-                .Map(source)
+            var result1 = Mapper
+                .Map(source1)
                 .ToANew<PublicField<int>>(c => c
                     .Map(ctx => ctx.Source.Value * 2)
                     .To(pf => pf.Value));
 
-            result.Value.ShouldBe(1 * 2);
+
+            var result2 = Mapper
+                .Map(source2)
+                .ToANew<PublicField<int>>(c => c
+                    .Map(ctx => ctx.Source.Value * 2)
+                    .To(pf => pf.Value));
+
+            result1.Value.ShouldBe(source1.Value * 2);
+            result2.Value.ShouldBe(source2.Value * 2);
         }
     }
 }
--- AgileMapper/Extensions/ExpressionEquator.cs
+++ AgileMapper/Extensions/ExpressionEquator.cs
@@ -1,5 +1,6 @@
 namespace AgileObjects.AgileMapper.Extensions
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq.Expressions;
     using System.Reflection;
@@ -10,35 +11,82 @@ namespace AgileObjects.AgileMapper.Extensions
 
         public bool Equals(Expression x, Expression y)
         {
-            if (x.NodeType != y.NodeType)
+            while (true)
+            {
+                if (x.NodeType != y.NodeType)
+                {
+                    return false;
+                }
+
+                switch (x.NodeType)
+                {
+                    case ExpressionType.Call:
+                        return AreEqual((MethodCallExpression)x, (MethodCallExpression)y);
+
+                    case ExpressionType.Constant:
+                        return AreEqual((ConstantExpression)x, (ConstantExpression)y);
+
+                    case ExpressionType.Index:
+                        return AreEqual((IndexExpression)x, (IndexExpression)y);
+
+                    case ExpressionType.Lambda:
+                        x = ((LambdaExpression)x).Body;
+                        y = ((LambdaExpression)y).Body;
+                        continue;
+
+                    case ExpressionType.MemberAccess:
+                        return AreEqual((MemberExpression)x, (MemberExpression)y);
+
+                    case ExpressionType.Multiply:
+                        return AreEqual((BinaryExpression)x, (BinaryExpression)y);
+
+                    case ExpressionType.Parameter:
+                        return AreEqual((ParameterExpression)x, (ParameterExpression)y);
+
+                    case ExpressionType.Quote:
+                        x = ((UnaryExpression)x).Operand;
+                        y = ((UnaryExpression)y).Operand;
+                        continue;
+                }
+
+                return false;
+            }
+        }
+
+        private bool AllEqual(IList<Expression> xItems, IList<Expression> yItems)
+        {
+            if (xItems.Count != yItems.Count)
             {
                 return false;
             }
 
-            switch (x.NodeType)
+            for (var i = 0; i < xItems.Count; i++)
             {
-                case ExpressionType.Index:
-                    return AreEqual((IndexExpression)x, (IndexExpression)y);
-
-
-                case ExpressionType.MemberAccess:
-                    return AreEqual((MemberExpression)x, (MemberExpression)y);
+                if (!Equals(xItems[i], yItems[i]))
+                {
+                    return false;
+                }
             }
 
-            return false;
+            return true;
+        }
+
+        private bool AreEqual(MethodCallExpression x, MethodCallExpression y)
+        {
+            return ReferenceEquals(x.Method, y.Method) &&
+                   (((x.Object == null) && (y.Object == null)) || ((x.Object != null) && Equals(x.Object, y.Object))) &&
+                   AllEqual(x.Arguments, y.Arguments);
+        }
+
+        private static bool AreEqual(ConstantExpression x, ConstantExpression y)
+        {
+            return x.Value.Equals(y.Value);
         }
 
         private bool AreEqual(IndexExpression x, IndexExpression y)
         {
-            if (!ReferenceEquals(x.Indexer, y.Indexer))
-            {
-                return false;
-            }
-
-            var xIndex = (ConstantExpression)x.Arguments[0];
-            var yIndex = (ConstantExpression)y.Arguments[0];
-
-            return xIndex.Value.Equals(yIndex.Value);
+            return ReferenceEquals(x.Indexer, y.Indexer) &&
+                   AllEqual(x.Arguments, y.Arguments);
         }
 
         private bool AreEqual(MemberExpression x, MemberExpression y)
@@ -48,13 +96,19 @@ namespace AgileObjects.AgileMapper.Extensions
                 return true;
             }
 
-            if (x.Member.Name != y.Member.Name)
-            {
-                return false;
-            }
+            return (x.Member.Name == y.Member.Name) &&
+                    y.Member.DeclaringType.IsAssignableFrom(x.Member.DeclaringType);
+        }
 
-            // ReSharper disable once PossibleNullReferenceException
-            return y.Member.DeclaringType.IsAssignableFrom(x.Member.DeclaringType);
+        private bool AreEqual(BinaryExpression x, BinaryExpression y)
+        {
+            return ReferenceEquals(x.Method, y.Method) &&
+                   Equals(x.Left, y.Left) && Equals(x.Right, y.Right);
+        }
+
+        private static bool AreEqual(ParameterExpression x, ParameterExpression y)
+        {
+            return (x.Type == y.Type) && (x.Name == y.Name);
         }
 
         public int GetHashCode(Expression obj) => 0;
