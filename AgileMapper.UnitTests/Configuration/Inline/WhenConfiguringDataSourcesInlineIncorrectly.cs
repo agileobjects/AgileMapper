2d0e5eb57649f26dbac06c91cbe07b85ee6a2973
Steve Wilkes <steve@agileobjects.co.uk>
2017-10-22 13:09:42 +0100

    Test coverage for invalid inline data source and member ignore configuration

---
+++ AgileMapper.UnitTests/Configuration/Inline/WhenConfiguringDataSourcesInlineIncorrectly.cs
@@ -0,0 +1,55 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Configuration.Inline
+{
+    using AgileMapper.Configuration;
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenConfiguringDataSourcesInlineIncorrectly
+    {
+        [Fact]
+        public void ShouldErrorIfDuplicateDataSourceIsConfiguredInline()
+        {
+            var inlineConfigEx = Should.Throw<MappingConfigurationException>(() =>
+            {
+                using (var mapper = Mapper.CreateNew())
+                {
+                    mapper.WhenMapping
+                        .From<PublicField<string>>()
+                        .To<PublicField<string>>()
+                        .Map(ctx => ctx.Source.Value + "!")
+                        .To(pf => pf.Value);
+
+                    mapper
+                        .Map(new PublicField<string> { Value = "Hello" })
+                        .ToANew<PublicField<string>>(cfg => cfg
+                            .Map(ctx => ctx.Source.Value + "!")
+                            .To(pf => pf.Value));
+                }
+            });
+
+            inlineConfigEx.Message.ShouldContain("already has that configured data source");
+        }
+
+        [Fact]
+        public void ShouldErrorIfIgnoredMemberHasDataSourceConfiguredInline()
+        {
+            var inlineConfigEx = Should.Throw<MappingConfigurationException>(() =>
+            {
+                using (var mapper = Mapper.CreateNew())
+                {
+                    mapper
+                        .Map(new PublicField<string> { Value = "Hello" })
+                        .ToANew<PublicField<string>>(
+                            cfg => cfg
+                                .Ignore(pf => pf.Value),
+                            cfg => cfg
+                                .Map(ctx => ctx.Source.Value + "?!")
+                                .To(pf => pf.Value));
+                }
+            });
+
+            inlineConfigEx.Message.ShouldContain("Ignored member Target.Value has a configured data source");
+        }
+    }
+}
---
+++ AgileMapper.UnitTests/Configuration/Inline/WhenIgnoringMembersInlineIncorrectly.cs
@@ -0,0 +1,30 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Configuration.Inline
+{
+    using AgileMapper.Configuration;
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenIgnoringMembersInlineIncorrectly
+    {
+        [Fact]
+        public void ShouldErrorIfConfiguredDataSourceMemberIsIgnoredInline()
+        {
+            var inlineConfigEx = Should.Throw<MappingConfigurationException>(() =>
+            {
+                using (var mapper = Mapper.CreateNew())
+                {
+                    mapper
+                        .Map(new PublicField<string> { Value = "Hello" })
+                        .ToANew<PublicField<string>>(cfg => cfg
+                            .Map(ctx => ctx.Source.Value + "?!")
+                            .To(pf => pf.Value)
+                            .And
+                            .Ignore(pf => pf.Value));
+                }
+            });
+
+            inlineConfigEx.Message.ShouldContain("Ignored member Target.Value has a configured data source");
+        }
+    }
+}
