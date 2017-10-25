317af85c8e8026d9ba928ae2ec4a728623e32540
Steve Wilkes <steve@agileobjects.co.uk>
2017-10-25 16:13:40 +0100

    Start of test coverage for inline derived type mapping configuration

---
+++ AgileMapper.UnitTests/Configuration/Inline/WhenConfiguringDerivedTypesInline.cs
@@ -0,0 +1,34 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Configuration.Inline
+{
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenConfiguringDerivedTypesInline
+    {
+        [Fact]
+        public void ShouldMapACustomTypePairInline()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                Product product = new MegaProduct
+                {
+                    ProductId = "Pretty-Darn-Mega",
+                    Price = 99.99,
+                    HowMega = 0.99m
+                };
+
+                var result = mapper
+                    .Map(product)
+                    .ToANew<ProductDto>(cfg => cfg
+                        .Map<MegaProduct>()
+                        .To<ProductDtoMega>());
+
+                result.ShouldBeOfType<ProductDtoMega>();
+                result.ProductId.ShouldBe("Pretty-Darn-Mega");
+                result.Price.ShouldBe(99.99m);
+                ((ProductDtoMega)result).HowMega.ShouldBe("0.99");
+            }
+        }
+    }
+}
