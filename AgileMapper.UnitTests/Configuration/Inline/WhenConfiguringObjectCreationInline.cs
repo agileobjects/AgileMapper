71d18bc5fe33c88e0dc93c2e6dac258553b4c381
Steve Wilkes <steve@agileobjects.co.uk>
2017-10-25 13:14:54 +0100

    Extra test coverage for inline object factory configuration

--- AgileMapper.UnitTests/Configuration/Inline/WhenConfiguringObjectCreationInline.cs
+++ AgileMapper.UnitTests/Configuration/Inline/WhenConfiguringObjectCreationInline.cs
@@ -34,5 +34,49 @@
                 mapper.InlineContexts().ShouldHaveSingleItem();
             }
         }
+
+        [Fact]
+        public void ShouldReplaceATargetInstanceFactory()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                mapper.WhenMapping
+                    .ToANew<Address>()
+                    .CreateInstancesUsing(ctx => new Address { Line2 = "Line 2!" })
+                    .And
+                    .Ignore(a => a.Line2);
+
+                var result = mapper
+                    .Map(new Address { Line1 = "Line 1!" })
+                    .ToANew<Address>(cfg => cfg
+                        .CreateInstancesUsing(ctx => new Address { Line2 = "Line 2?!" }));
+
+                result.Line1.ShouldBe("Line 1!");
+                result.Line2.ShouldBe("Line 2?!");
+            }
+        }
+
+        [Fact]
+        public void ShouldReplaceATargetInstanceFactoryForANestedObject()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                mapper.WhenMapping
+                    .Over<Address>()
+                    .CreateInstancesUsing(ctx => new Address { Line2 = "Line 2!" })
+                    .And
+                    .Ignore(a => a.Line2);
+
+                var result = mapper
+                    .Map(new CustomerViewModel { Name = "Me", AddressLine1 = "Line 1!" })
+                    .Over(new Customer { Name = "You" }, cfg => cfg
+                           .CreateInstancesOf<Address>().Using(ctx => new Address { Line2 = "Line 2?!" }));
+
+                result.Name.ShouldBe("Me");
+                result.Address.ShouldNotBeNull();
+                result.Address.Line1.ShouldBe("Line 1!");
+                result.Address.Line2.ShouldBe("Line 2?!");
+            }
+        }
     }
 }
