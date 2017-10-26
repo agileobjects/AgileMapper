ea0664809226fc6123c0c97cb039b5ae74725564
Steve Wilkes <steve@agileobjects.co.uk>
2017-10-26 11:50:14 +0100

    Start of test coverage for inline-configured mapping callbacks

---
+++ AgileMapper.UnitTests/Configuration/Inline/WhenConfiguringCallbacksInline.cs
@@ -0,0 +1,61 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Configuration.Inline
+{
+    using System;
+    using AgileMapper.Members;
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenConfiguringCallbacksInline
+    {
+        [Fact]
+        public void ShouldConfigureExceptionSwallowingInline()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                var result = mapper
+                    .Map(new Person())
+                    .ToANew<PersonViewModel>(cfg => cfg
+                        .SwallowAllExceptions()
+                        .And
+                        .After
+                        .CreatingInstances
+                        .Call(ctx => FallOver()));
+
+                result.ShouldBeNull();
+            }
+        }
+
+        [Fact]
+        public void ShouldConfigureAnExceptionCallbackInline()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                var thrownData = default(IMappingExceptionData<Person, PersonViewModel>);
+
+                mapper
+                    .Map(new Person())
+                    .ToANew<PersonViewModel>(cfg => cfg
+                        .PassExceptionsTo(ctx => SetContext(ctx, out thrownData))
+                        .And
+                        .After.CreatingInstances
+                        .Call(ctx => FallOver()));
+
+                thrownData.ShouldNotBeNull();
+                thrownData.Source.ShouldBeOfType<Person>();
+                thrownData.Target.ShouldBeOfType<PersonViewModel>();
+                thrownData.Exception.ShouldNotBeNull();
+                thrownData.Exception.Message.ShouldBe("BANG");
+            }
+        }
+
+        private static void SetContext(
+            IMappingExceptionData<Person, PersonViewModel> thrownData,
+            out IMappingExceptionData<Person, PersonViewModel> outData)
+        {
+            outData = thrownData;
+        }
+
+        private static void FallOver() => throw new InvalidOperationException("BANG");
+    }
+}
