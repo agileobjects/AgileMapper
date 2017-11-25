206af9bc37fa1ca9f79e47201494da739426aaf9
Steve Wilkes <steve@agileobjects.co.uk>
2017-11-25 12:22:10 +0000

    Test coverage for projection query mapping plan caching in EF Core 1

---
+++ AgileMapper.UnitTests.Orms.EfCore1/WhenViewingMappingPlans.cs
@@ -0,0 +1,18 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Orms.EfCore1
+{
+    using System;
+    using Infrastructure;
+    using Microsoft.EntityFrameworkCore.Query.Internal;
+    using Queryables.Api;
+
+    public class WhenViewingMappingPlans : WhenViewingMappingPlans<EfCore1TestDbContext>
+    {
+        public WhenViewingMappingPlans(InMemoryEfCore1TestContext context)
+            : base(context)
+        {
+        }
+
+        protected override Type GetQueryProviderType(QueryProviderTypeSelector selector)
+            => selector.Using<EntityQueryProvider>();
+    }
+}
