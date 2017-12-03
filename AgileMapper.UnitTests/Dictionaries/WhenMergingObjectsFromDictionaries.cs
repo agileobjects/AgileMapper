82aef9cf3ffd76c70dd842f2fa593af69717466b
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-03 07:57:00 +0000

    Test coverage for merging a non-Dictionary<,> IDictionary<,> implementation on to a complex type array

--- AgileMapper.UnitTests/Dictionaries/WhenMergingObjectsFromDictionaries.cs
+++ AgileMapper.UnitTests/Dictionaries/WhenMergingObjectsFromDictionaries.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Shouldly;
     using TestClasses;
     using Xunit;
@@ -18,6 +19,31 @@
             var result = Mapper.Map(source).OnTo(target);
 
             result.Value.ShouldBe(guid.ToString());
+        }
+
+        [Fact]
+        public void ShouldMergeANestedComplexTypeArrayFromUntypedDictionaryImplementationEntries()
+        {
+            var source = new StringKeyedDictionary<object>
+            {
+                ["Value[0]"] = new CustomerViewModel { Name = "Mr Pants" },
+                ["Value[1]"] = new PersonViewModel { Name = "Ms Blouse" }
+            };
+            var target = new PublicProperty<PersonViewModel[]>
+            {
+                Value = new PersonViewModel[]
+                {
+                    new CustomerViewModel { Name = "Sir Trousers", Discount = 0.99 }
+                }
+            };
+            var originalValue1 = target.Value.First();
+            var result = Mapper.Map(source).OnTo(target);
+
+            result.Value.Length.ShouldBe(3);
+
+            result.Value.First().ShouldBe(originalValue1);
+            result.Value.Second().Name.ShouldBe("Mr Pants");
+            result.Value.Third().Name.ShouldBe("Ms Blouse");
         }
 
         [Fact]
