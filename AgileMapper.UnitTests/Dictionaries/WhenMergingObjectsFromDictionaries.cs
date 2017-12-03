438c3da382ac6ba4d12cde0437b79bacd5376ba3
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-03 07:46:48 +0000

    Organising source Dictionary mapping tests

---
+++ AgileMapper.UnitTests/Dictionaries/WhenMergingObjectsFromDictionaries.cs
@@ -0,0 +1,34 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Dictionaries
+{
+    using System;
+    using System.Collections.Generic;
+    using Shouldly;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenMergingObjectsFromDictionaries
+    {
+        [Fact]
+        public void ShouldPopulateAStringMemberFromANullableTypedEntry()
+        {
+            var guid = Guid.NewGuid();
+
+            var source = new Dictionary<string, Guid?> { ["Value"] = guid };
+            var target = new PublicProperty<string>();
+            var result = Mapper.Map(source).OnTo(target);
+
+            result.Value.ShouldBe(guid.ToString());
+        }
+
+        [Fact]
+        public void ShouldReuseAnExistingListIfNoEntriesMatch()
+        {
+            var source = new Dictionary<string, object>();
+            var target = new PublicProperty<ICollection<string>> { Value = new List<string>() };
+            var originalList = target.Value;
+            var result = Mapper.Map(source).OnTo(target);
+
+            result.Value.ShouldBeSameAs(originalList);
+        }
+    }
+}
