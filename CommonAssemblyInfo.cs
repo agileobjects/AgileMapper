5cc4e7777abf93bb2db64a8a208819fc0dd5eb09
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-05 18:45:44 +0000

    Adding a common properties file / Updating metadata

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Resources;
 
 [assembly: AssemblyCompany("AgileObjects Ltd")]
 [assembly: AssemblyProduct("AgileObjects.AgileMapper")]
-[assembly: AssemblyCopyright("Copyright © AgileObjects Ltd 2016")]
+[assembly: AssemblyCopyright("Copyright © AgileObjects Ltd 2017")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: CLSCompliant(true)]
