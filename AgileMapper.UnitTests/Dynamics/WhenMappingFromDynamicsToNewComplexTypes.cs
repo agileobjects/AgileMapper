bf8282d87f88af490ac21c24365841b6ef3e3df7
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-10 10:26:27 +0000

    Test coverage for namming ExpandoObject (not IDictionary<,>) in thrown MappingExceptions

--- AgileMapper.UnitTests/Dynamics/WhenMappingFromDynamicsToNewComplexTypes.cs
+++ AgileMapper.UnitTests/Dynamics/WhenMappingFromDynamicsToNewComplexTypes.cs
@@ -1,5 +1,6 @@
 ﻿namespace AgileObjects.AgileMapper.UnitTests.Dynamics
 {
+    using System;
     using System.Dynamic;
     using Shouldly;
     using TestClasses;
@@ -11,6 +12,7 @@
         public void ShouldMapToASimpleTypeMember()
         {
             dynamic source = new ExpandoObject();
+
             source.value = 123;
 
             var result = (PublicField<int>)Mapper.Map(source).ToANew<PublicField<int>>();
@@ -22,6 +24,7 @@
         public void ShouldConvertASimpleTypeMemberValue()
         {
             dynamic source = new ExpandoObject();
+
             source.Value = "728";
 
             var result = (PublicField<long>)Mapper.Map(source).ToANew<PublicField<long>>();
@@ -32,12 +35,33 @@
         [Fact]
         public void ShouldHandleANullASimpleTypeMemberValue()
         {
-            dynamic source = new ExpandoObject{};
+            dynamic source = new ExpandoObject();
+
             source.Value = default(string);
 
             var result = (PublicSetMethod<string>)Mapper.Map(source).ToANew<PublicSetMethod<string>>();
 
             result.Value.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldWrapAMappingException()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                dynamic source = new ExpandoObject();
+
+                source.ValueLine1 = "1 Exception Road";
+
+                mapper.Before
+                    .CreatingInstancesOf<Address>()
+                    .Call(ctx => throw new InvalidOperationException("I DON'T LIKE ADDRESSES"));
+
+                var mappingEx = Should.Throw<MappingException>(() =>
+                    mapper.Map(source).ToANew<PublicField<Address>>());
+
+                mappingEx.Message.ShouldContain(nameof(ExpandoObject));
+            }
         }
     }
 }
