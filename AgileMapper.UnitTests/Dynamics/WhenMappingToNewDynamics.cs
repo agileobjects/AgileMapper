56f81086d3a4a1251050a1019332f38136e6c68a
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-12 18:46:27 +0000

    Extending map-to-dynamic test coverage

--- AgileMapper.UnitTests/Dynamics/WhenMappingToNewDynamics.cs
+++ AgileMapper.UnitTests/Dynamics/WhenMappingToNewDynamics.cs
@@ -1,17 +1,27 @@
 ﻿namespace AgileObjects.AgileMapper.UnitTests.Dynamics
 {
+    using System.Dynamic;
     using Shouldly;
     using Xunit;
 
     public class WhenMappingToNewDynamics
     {
         [Fact]
-        public void ShouldMapToASimpleTypeMember()
+        public void ShouldMapToADynamicSimpleTypeMember()
         {
             var result = Mapper.Map(new { Value = 123 }).ToANew<dynamic>();
 
             ((object)result).ShouldNotBeNull();
             ((int)result.Value).ShouldBe(123);
+        }
+
+        [Fact]
+        public void ShouldMapToAnExpandoObjectSimpleTypeMember()
+        {
+            dynamic result = Mapper.Map(new { Value = "Oh so dynamic" }).ToANew<ExpandoObject>();
+
+            ((object)result).ShouldNotBeNull();
+            ((string)result.Value).ShouldBe("Oh so dynamic");
         }
     }
 }
