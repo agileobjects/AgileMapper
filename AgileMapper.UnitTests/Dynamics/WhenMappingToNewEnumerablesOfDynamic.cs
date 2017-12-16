87253c0d7cc578d99b15e034e1c77ee3051ecd1d
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-16 07:55:45 +0000

    Test coverage for mapping to root enumerable<dynamic>

---
+++ AgileMapper.UnitTests/Dynamics/WhenMappingToNewEnumerablesOfDynamic.cs
@@ -0,0 +1,28 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Dynamics
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenMappingToNewEnumerablesOfDynamic
+    {
+        [Fact]
+        public void ShouldMapAComplexTypeArray()
+        {
+            var source = new[]
+            {
+                new PublicField<char> { Value = '1' },
+                new PublicField<char> { Value = '2' },
+                new PublicField<char> { Value = '3' }
+            };
+            var result = Mapper.Map(source).ToANew<ICollection<dynamic>>();
+
+            result.Count.ShouldBe(3);
+
+            ((char)result.First().Value).ShouldBe('1');
+            ((char)result.Second().Value).ShouldBe('2');
+            ((char)result.Third().Value).ShouldBe('3');
+        }
+    }
+}
