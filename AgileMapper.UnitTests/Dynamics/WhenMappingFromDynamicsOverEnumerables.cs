69f643c868796354320b4cb0ccec97f21d0c83e2
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-18 19:34:38 +0000

    Extending dynamic over enumerables test coverage

--- AgileMapper.UnitTests/Dynamics/WhenMappingFromDynamicsOverEnumerables.cs
+++ AgileMapper.UnitTests/Dynamics/WhenMappingFromDynamicsOverEnumerables.cs
@@ -2,6 +2,9 @@
 {
     using System.Collections.Generic;
     using System.Dynamic;
+    using System.Linq;
+    using AgileMapper.Extensions.Internal;
+    using Shouldly;
     using TestClasses;
     using Xunit;
 
@@ -22,6 +25,43 @@
             Mapper.Map(source).Over(target);
 
             target.Value.ShouldBe(4L, 5L, 6L);
+        }
+
+        [Fact]
+        public void ShouldMapToAComplexTypeArrayFromAConvertibleTypedSourceEnumerable()
+        {
+            dynamic source = new ExpandoObject();
+
+            source.Value = new[]
+            {
+                new Person { Name = "Mr Pants"},
+                new Customer { Name = "Mrs Blouse" }
+            };
+
+            var target = new PublicProperty<PersonViewModel[]>();
+
+            Mapper.Map(source).Over(target);
+
+            target.Value.Length.ShouldBe(2);
+            target.Value.First().Name.ShouldBe("Mr Pants");
+            target.Value.Second().Name.ShouldBe("Mrs Blouse");
+        }
+
+        [Fact]
+        public void ShouldMapToAComplexTypeEnumerableFromFlattenedEntries()
+        {
+            dynamic source = new ExpandoObject();
+
+            source._0_ProductId = "Hose";
+            source._0_Price = "1.99";
+
+            IEnumerable<Product> target = new List<Product>();
+
+            Mapper.Map(source).Over(target);
+
+            target.ShouldHaveSingleItem();
+            target.First().ProductId.ShouldBe("Hose");
+            target.First().Price.ShouldBe(1.99);
         }
     }
 }
