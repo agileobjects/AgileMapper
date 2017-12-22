c5aea0cf9b6639aa531f3036e07e77d2ece02815
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-22 08:08:12 +0000

    Test coverage for using the DeepClone extension method with a specified mapper

--- AgileMapper.UnitTests/Extensions/WhenMappingToNewComplexTypes.cs
+++ AgileMapper.UnitTests/Extensions/WhenMappingToNewComplexTypes.cs
@@ -15,5 +15,28 @@
             result.ShouldNotBeNull();
             result.Value.ShouldBe(123);
         }
+
+        [Fact]
+        public void ShouldUseASpecifiedMapperInADeepClone()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                mapper.WhenMapping
+                    .From<PublicTwoFieldsStruct<int, int>>()
+                    .To<PublicTwoFieldsStruct<int, int>>()
+                    .Map(ctx => ctx.Source.Value1)
+                    .To(p => p.Value2)
+                    .And
+                    .Map(ctx => ctx.Source.Value2)
+                    .To(p => p.Value1);
+
+                var source = new PublicTwoFieldsStruct<int, int> { Value1 = 123, Value2 = 456 };
+                var result = source.DeepClone(_ => _.Using(mapper));
+
+                result.ShouldNotBe(source);
+                result.Value1.ShouldBe(456);
+                result.Value2.ShouldBe(123);
+            }
+        }
     }
 }
