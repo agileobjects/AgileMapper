4f22dd868c1cd1ab4b155c4bba9aa9d3b10e6225
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-22 17:54:46 +0000

    Start of test coverage for .Flatten().ToQueryString() extension method

---
+++ AgileMapper.UnitTests/Extensions/WhenFlatteningToQueryStringViaExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Extensions
+{
+    using AgileMapper.Extensions;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenFlatteningToQueryStringViaExtensionMethods
+    {
+        [Fact]
+        public void ShouldFlattenToQueryString()
+        {
+            var source = new Address { Line1 = "Here", Line2 = "There" };
+            var result = source.Flatten().ToQueryString();
+
+            result.ShouldBe("Line1=Here&Line2=There");
+        }
+
+        [Fact]
+        public void ShouldFlattenToQueryStringWithASpecifiedMapper()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                var callbackCalled = false;
+
+                mapper.After.MappingEnds.Call(ctx => callbackCalled = true);
+
+                var source = new Address { Line1 = "Here", Line2 = "There" };
+                var result = source.Flatten(_ => _.Using(mapper)).ToQueryString();
+
+                callbackCalled.ShouldBeTrue();
+                result.ShouldBe("Line1=Here&Line2=There");
+            }
+        }
+    }
+}
--- AgileMapper.UnitTests/Extensions/WhenFlatteningViaExtensionMethods.cs
+++ AgileMapper.UnitTests/Extensions/WhenFlatteningViaExtensionMethods.cs
@@ -1,6 +1,5 @@
 ﻿namespace AgileObjects.AgileMapper.UnitTests.Extensions
 {
-    using System.Runtime.InteropServices;
     using AgileMapper.Extensions;
     using TestClasses;
     using Xunit;
@@ -52,6 +51,104 @@
                 ((string)result._1Line2).ShouldBe("2_2");
                 ((string)result._2Line1).ShouldBe("3_1");
                 ((string)result._2Line2).ShouldBe("3_2");
+            }
+        }
+
+        [Fact]
+        public void ShouldFlattenToDictionary()
+        {
+            var source = new[]
+            {
+                new Address { Line1 = "1_1", Line2 = "1_2" },
+                new Address { Line1 = "2_1", Line2 = "2_2" },
+                new Address { Line1 = "3_1", Line2 = "3_2" }
+            };
+
+            var result = source.Flatten().ToDictionary();
+
+            result["[0].Line1"].ShouldBe("1_1");
+            result["[0].Line2"].ShouldBe("1_2");
+            result["[1].Line1"].ShouldBe("2_1");
+            result["[1].Line2"].ShouldBe("2_2");
+            result["[2].Line1"].ShouldBe("3_1");
+            result["[2].Line2"].ShouldBe("3_2");
+        }
+
+        [Fact]
+        public void ShouldFlattenToDictionaryWithASpecifiedMapper()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                mapper.WhenMapping
+                    .From<Address[]>()
+                    .ToDictionaries
+                    .UseElementKeyPattern("-i-")
+                    .UseFlattenedMemberNames();
+
+                var source = new[]
+                {
+                    new Address { Line1 = "1_1", Line2 = "1_2" },
+                    new Address { Line1 = "2_1", Line2 = "2_2" },
+                    new Address { Line1 = "3_1", Line2 = "3_2" }
+                };
+
+                var result = source.Flatten(_ => _.Using(mapper)).ToDictionary();
+
+                result["-0-Line1"].ShouldBe("1_1");
+                result["-0-Line2"].ShouldBe("1_2");
+                result["-1-Line1"].ShouldBe("2_1");
+                result["-1-Line2"].ShouldBe("2_2");
+                result["-2-Line1"].ShouldBe("3_1");
+                result["-2-Line2"].ShouldBe("3_2");
+            }
+        }
+
+        [Fact]
+        public void ShouldFlattenToStringDictionary()
+        {
+            var source = new[]
+            {
+                new Address { Line1 = "1_1", Line2 = "1_2" },
+                new Address { Line1 = "2_1", Line2 = "2_2" },
+                new Address { Line1 = "3_1", Line2 = "3_2" }
+            };
+
+            var result = source.Flatten().ToDictionary<string>();
+
+            result["[0].Line1"].ShouldBe("1_1");
+            result["[0].Line2"].ShouldBe("1_2");
+            result["[1].Line1"].ShouldBe("2_1");
+            result["[1].Line2"].ShouldBe("2_2");
+            result["[2].Line1"].ShouldBe("3_1");
+            result["[2].Line2"].ShouldBe("3_2");
+        }
+
+        [Fact]
+        public void ShouldFlattenToStringDictionaryWithASpecifiedMapper()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                mapper.WhenMapping
+                    .From<Address[]>()
+                    .ToDictionaries
+                    .UseElementKeyPattern("-i-")
+                    .UseFlattenedMemberNames();
+
+                var source = new[]
+                {
+                    new Address { Line1 = "1_1", Line2 = "1_2" },
+                    new Address { Line1 = "2_1", Line2 = "2_2" },
+                    new Address { Line1 = "3_1", Line2 = "3_2" }
+                };
+
+                var result = source.Flatten(_ => _.Using(mapper)).ToDictionary<string>();
+
+                result["-0-Line1"].ShouldBe("1_1");
+                result["-0-Line2"].ShouldBe("1_2");
+                result["-1-Line1"].ShouldBe("2_1");
+                result["-1-Line2"].ShouldBe("2_2");
+                result["-2-Line1"].ShouldBe("3_1");
+                result["-2-Line2"].ShouldBe("3_2");
             }
         }
     }
