51ce626a28fc116f4a64127c81aab44fa4336ef4
Steve Wilkes <steve@agileobjects.co.uk>
2018-01-10 19:54:10 +0000

    Start of flags enum support

---
+++ AgileMapper.UnitTests/SimpleTypeConversion/WhenConvertingToFlagsEnums.cs
@@ -0,0 +1,17 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.SimpleTypeConversion
+{
+    using TestClasses;
+    using Xunit;
+
+    public class WhenConvertingToFlagsEnums
+    {
+        [Fact]
+        public void ShouldMapASingleValueByteToAFlagsEnum()
+        {
+            var source = new PublicField<byte> { Value = (byte)Status.InProgress };
+            var result = Mapper.Map(source).ToANew<PublicField<Status>>();
+
+            result.Value.ShouldBe(Status.InProgress);
+        }
+    }
+}
---
+++ AgileMapper.UnitTests/TestClasses/Status.cs
@@ -0,0 +1,15 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.TestClasses
+{
+    using System;
+
+    [Flags]
+    public enum Status
+    {
+        New = 1,
+        Assigned = 2,
+        InProgress = 4,
+        Complete = 8,
+        Cancelled = 16,
+        Removed = 32
+    }
+}
