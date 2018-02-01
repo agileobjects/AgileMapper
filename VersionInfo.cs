26be41cd3379508e85925626789373265930d4d7
Steve Wilkes <steve@agileobjects.co.uk>
2018-02-01 20:36:19 +0000

    Updating to v0.20

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.19.0")]
-[assembly: AssemblyFileVersion("0.19.0")]
+[assembly: AssemblyVersion("0.20.0")]
+[assembly: AssemblyFileVersion("0.20.0")]
