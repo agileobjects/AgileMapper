20fb65da4529966574bc79fed009d826251306d6
Steve Wilkes <steve@agileobjects.co.uk>
2018-02-13 08:44:23 +0000

    Moving callback tests to EFCore-only / Test coverage for object tracking + projection

--- AgileMapper.UnitTests.Orms.EfCore2/Configuration/WhenConfiguringCallbacks.cs
+++ AgileMapper.UnitTests.Orms.EfCore2/Configuration/WhenConfiguringCallbacks.cs
@@ -1,13 +1,237 @@
 ﻿namespace AgileObjects.AgileMapper.UnitTests.Orms.EfCore2.Configuration
 {
+    using System.Threading.Tasks;
     using Infrastructure;
-    using Orms.Configuration;
+    using Orms.Infrastructure;
+    using TestClasses;
+    using Xunit;
 
-    public class WhenConfiguringCallbacks : WhenConfiguringCallbacks<EfCore2TestDbContext>
+    public class WhenConfiguringCallbacks : OrmTestClassBase<EfCore2TestDbContext>
     {
         public WhenConfiguringCallbacks(InMemoryEfCore2TestContext context)
             : base(context)
         {
+        }
+
+        [Fact]
+        public Task ShouldNotAttemptToCallAPreMappingCallback()
+        {
+            return RunTest(async context =>
+            {
+                using (var mapper = Mapper.CreateNew())
+                {
+                    var callbackCalled = false;
+
+                    mapper.WhenMapping
+                        .To<SquareViewModel>()
+                        .Before
+                        .MappingBegins
+                        .Call(ctx => callbackCalled = true);
+
+                    var square = new Square { SideLength = 12 };
+
+                    await context.Shapes.AddAsync(square);
+                    await context.SaveChangesAsync();
+
+                    var squareVm = context
+                        .Shapes
+                        .ProjectUsing(mapper)
+                        .To<SquareViewModel>()
+                        .ShouldHaveSingleItem();
+
+                    callbackCalled.ShouldBeFalse();
+                    squareVm.SideLength.ShouldBe(12);
+
+                    mapper.Map(square).OnTo(squareVm);
+
+                    callbackCalled.ShouldBeTrue();
+                }
+            });
+        }
+
+        [Fact]
+        public Task ShouldNotAttemptToCallAPostMappingCallback()
+        {
+            return RunTest(async context =>
+            {
+                using (var mapper = Mapper.CreateNew())
+                {
+                    var callbackCalled = false;
+
+                    mapper.WhenMapping
+                        .From<Shape>()
+                        .To<CircleViewModel>()
+                        .After
+                        .MappingEnds
+                        .Call((s, c) => callbackCalled = true);
+
+                    var circle = new Circle { Diameter = 1 };
+
+                    await context.Shapes.AddAsync(circle);
+                    await context.SaveChangesAsync();
+
+                    var circleVm = context
+                        .Shapes
+                        .ProjectUsing(mapper)
+                        .To<CircleViewModel>()
+                        .ShouldHaveSingleItem();
+
+                    callbackCalled.ShouldBeFalse();
+                    circleVm.Diameter.ShouldBe(1);
+
+                    mapper.Map(circle).Over(circleVm);
+
+                    callbackCalled.ShouldBeTrue();
+                }
+            });
+        }
+
+        [Fact]
+        public Task ShouldNotAttemptToCallAPreObjectCreationCallback()
+        {
+            return RunTest(async context =>
+            {
+                using (var mapper = Mapper.CreateNew())
+                {
+                    var callbackCalled = false;
+
+                    mapper.WhenMapping
+                        .To<PersonDto>()
+                        .Before
+                        .CreatingInstancesOf<AddressDto>()
+                        .Call(ctx => callbackCalled = true);
+
+                    var person = new Person { Name = "Bjorn", Address = new Address { Line1 = "Sweden" } };
+
+                    await context.Persons.AddAsync(person);
+                    await context.SaveChangesAsync();
+
+                    var personDto = context
+                        .Persons
+                        .ProjectUsing(mapper)
+                        .To<PersonDto>()
+                        .ShouldHaveSingleItem();
+
+                    callbackCalled.ShouldBeFalse();
+                    personDto.Name.ShouldBe("Bjorn");
+                    personDto.Address.ShouldNotBeNull();
+                    personDto.Address.Line1.ShouldBe("Sweden");
+
+                    mapper.Map(person).Over(personDto);
+
+                    callbackCalled.ShouldBeTrue();
+                }
+            });
+        }
+
+        [Fact]
+        public Task ShouldNotAttemptToCallAPostObjectCreationCallback()
+        {
+            return RunTest(async context =>
+            {
+                using (var mapper = Mapper.CreateNew())
+                {
+                    var callbackCalled = false;
+
+                    mapper.WhenMapping
+                        .To<PersonDto>()
+                        .After
+                        .CreatingInstances
+                        .Call(ctx => callbackCalled = true);
+
+                    var person = new Person { Name = "Benny", Address = new Address { Line1 = "Sweden" } };
+
+                    await context.Persons.AddAsync(person);
+                    await context.SaveChangesAsync();
+
+                    var personDto = context
+                        .Persons
+                        .ProjectUsing(mapper)
+                        .To<PersonDto>()
+                        .ShouldHaveSingleItem();
+
+                    callbackCalled.ShouldBeFalse();
+                    personDto.Name.ShouldBe("Benny");
+                    personDto.Address.ShouldNotBeNull();
+                    personDto.Address.Line1.ShouldBe("Sweden");
+
+                    mapper.Map(person).ToANew<PersonDto>();
+
+                    callbackCalled.ShouldBeTrue();
+                }
+            });
+        }
+
+        [Fact]
+        public Task ShouldNotAttemptToCallAPreMemberMappingCallback()
+        {
+            return RunTest(async context =>
+            {
+                using (var mapper = Mapper.CreateNew())
+                {
+                    var callbackCalled = false;
+
+                    mapper.WhenMapping
+                        .To<SquareViewModel>()
+                        .Before
+                        .Mapping(s => s.SideLength)
+                        .Call(ctx => callbackCalled = true);
+
+                    var square = new Square { SideLength = 1 };
+
+                    await context.Shapes.AddAsync(square);
+                    await context.SaveChangesAsync();
+
+                    var squareVm = context
+                        .Shapes
+                        .ProjectUsing(mapper)
+                        .To<SquareViewModel>()
+                        .ShouldHaveSingleItem();
+
+                    callbackCalled.ShouldBeFalse();
+                    squareVm.SideLength.ShouldBe(1);
+
+                    mapper.Map(square).Over(squareVm);
+
+                    callbackCalled.ShouldBeTrue();
+                }
+            });
+        }
+
+        [Fact]
+        public Task ShouldNotAttemptToCallAPostMemberMappingCallback()
+        {
+            return RunTest(async context =>
+            {
+                using (var mapper = Mapper.CreateNew())
+                {
+                    var callbackCalled = false;
+
+                    mapper.WhenMapping
+                        .To<CircleViewModel>()
+                        .After
+                        .Mapping(c => c.Diameter)
+                        .Call(ctx => callbackCalled = true);
+
+                    var circle = new Circle { Diameter = 11 };
+
+                    await context.Shapes.AddAsync(circle);
+                    await context.SaveChangesAsync();
+
+                    var circleVm = context
+                        .Shapes
+                        .ProjectUsing(mapper)
+                        .To<CircleViewModel>()
+                        .ShouldHaveSingleItem();
+
+                    callbackCalled.ShouldBeFalse();
+                    circleVm.Diameter.ShouldBe(11);
+
+                    mapper.Map(circle).Over(circleVm);
+
+                    callbackCalled.ShouldBeTrue();
+                }
+            });
         }
     }
 }
---
+++ AgileMapper.UnitTests.Orms.EfCore2/Configuration/WhenConfiguringObjectTracking.cs
@@ -0,0 +1,51 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Orms.EfCore2.Configuration
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+    using Orms.Infrastructure;
+    using TestClasses;
+    using Xunit;
+
+    public class WhenConfiguringObjectTracking : OrmTestClassBase<EfCore2TestDbContext>
+    {
+        public WhenConfiguringObjectTracking(InMemoryEfCore2TestContext context)
+            : base(context)
+        {
+        }
+
+        [Fact]
+        public Task ShouldIgnoreObjectTracking()
+        {
+            return RunTest(async context =>
+            {
+                var circle1 = new Circle { Diameter = 1 };
+                var circle2 = new Circle { Diameter = 2 };
+                var circle3 = new Circle { Diameter = 3 };
+
+                await context.Shapes.AddRangeAsync(circle1, circle2, circle3);
+                await context.SaveChangesAsync();
+
+                using (var mapper = Mapper.CreateNew())
+                {
+                    mapper.WhenMapping
+                        .To<CircleViewModel>()
+                        .MaintainIdentityIntegrity();
+
+                    var circleVms = await context
+                        .Shapes
+                        .ProjectUsing(mapper)
+                        .To<Circle>()
+                        .ToArrayAsync();
+
+                    circleVms.Length.ShouldBe(3);
+
+                    circleVms.First().Diameter.ShouldBe(1);
+                    circleVms.Second().Diameter.ShouldBe(2);
+                    circleVms.Third().Diameter.ShouldBe(3);
+                }
+            });
+        }
+    }
+}
