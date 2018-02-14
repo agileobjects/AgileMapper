46f2ec5629eec96251c542fd112b1999d752f899
Steve Wilkes <steve@agileobjects.co.uk>
2018-02-14 09:53:49 +0000

    Test coverage for inline-configured custom constructor data sources

---
+++ AgileMapper.UnitTests.Orms.EfCore1/Configuration/Inline/WhenConfiguringConstructorDataSourcesInline.cs
@@ -0,0 +1,20 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.Orms.EfCore1.Configuration.Inline
+{
+    using System.Threading.Tasks;
+    using Infrastructure;
+    using Orms.Configuration.Inline;
+    using Xunit;
+
+    public class WhenConfiguringConstructorDataSourcesInline 
+        : WhenConfiguringConstructorDataSourcesInline<EfCore1TestDbContext>
+    {
+        public WhenConfiguringConstructorDataSourcesInline(InMemoryEfCore1TestContext context)
+            : base(context)
+        {
+        }
+
+        [Fact]
+        public Task ShouldApplyAConfiguredConstantByParameterTypeInline()
+            => RunShouldApplyAConfiguredConstantByParameterTypeInline();
+    }
+}
