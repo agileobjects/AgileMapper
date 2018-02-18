e93b3fcfa0e91cbe4a29d6493c6196c33d59b41a
Steve Wilkes <steve@agileobjects.co.uk>
2018-02-18 18:48:19 +0000

    Extending Has<MemberName> test coverage

--- AgileMapper.UnitTests/WhenMappingToMetaMembers.cs
+++ AgileMapper.UnitTests/WhenMappingToMetaMembers.cs
@@ -1,10 +1,47 @@
 ﻿namespace AgileObjects.AgileMapper.UnitTests
 {
+    using System;
     using TestClasses;
     using Xunit;
 
     public class WhenMappingToMetaMembers
     {
+        [Fact]
+        public void ShouldPopulateAHasStringMemberNameMember()
+        {
+            var source = new PublicField<string> { Value = "Yessir!" };
+            var result = Mapper.Map(source).ToANew<PublicHasValue<string>>();
+
+            result.HasValue.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ShouldPopulateAHasStringMemberNameMemberWithFalse()
+        {
+            var source = new PublicField<string> { Value = default(string) };
+            var result = Mapper.Map(source).ToANew<PublicHasValue<string>>();
+
+            result.HasValue.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ShouldPopulateAHasDateTimeMemberNameMember()
+        {
+            var source = new PublicField<DateTime> { Value = DateTime.Now };
+            var result = Mapper.Map(source).ToANew<PublicHasValue<DateTime>>();
+
+            result.HasValue.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ShouldPopulateAHasDateTimeMemberNameMemberWithFalse()
+        {
+            var source = new PublicField<DateTime> { Value = default(DateTime) };
+            var result = Mapper.Map(source).ToANew<PublicHasValue<DateTime>>();
+
+            result.HasValue.ShouldBeFalse();
+        }
+
         [Fact]
         public void ShouldPopulateAHasComplexTypeMemberNameMember()
         {
@@ -12,6 +49,15 @@
             var result = Mapper.Map(source).ToANew<PublicHasValue<Address>>();
 
             result.HasValue.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ShouldPopulateAHasComplexTypeMemberNameMemberWithFalse()
+        {
+            var source = new PublicField<PublicField<int>> { Value = default(PublicField<int>) };
+            var result = Mapper.Map(source).ToANew<PublicHasValue<PublicField<int>>>();
+
+            result.HasValue.ShouldBeFalse();
         }
 
         #region Helper Classes
