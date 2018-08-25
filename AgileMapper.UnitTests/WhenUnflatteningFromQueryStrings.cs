7cbe4e081cb14c80776ed3a6f1dc0ad36f5640f6
Steve Wilkes <SteveWilkes@users.noreply.github.com>
2018-08-25 08:51:12 +0100

    Adding Unflatten().To(Type) overload (#88)

--- AgileMapper.UnitTests/WhenUnflatteningFromQueryStrings.cs
+++ AgileMapper.UnitTests/WhenUnflatteningFromQueryStrings.cs
@@ -1,6 +1,7 @@
 ﻿namespace AgileObjects.AgileMapper.UnitTests
 {
     using System;
+    using System.Collections.ObjectModel;
     using AgileMapper.Extensions;
     using TestClasses;
 #if !NET35
@@ -30,7 +31,7 @@
             using (var mapper = Mapper.CreateNew())
             {
                 var queryString = "%5B0%5D%2EValue=1&%5B1%5D%2EValue=2&%5B2%5D%2EValue=3".ToQueryString();
-                
+
                 var result = mapper.Unflatten(queryString).To<PublicProperty<int>[]>();
 
                 result.ShouldNotBeNull();
@@ -39,6 +40,23 @@
                 result[1].Value.ShouldBe(2);
                 result[2].Value.ShouldBe(3);
             }
+        }
+
+        // See https://github.com/agileobjects/AgileMapper/issues/87
+        [Fact]
+        public void ShouldUnflattenToARuntimeType()
+        {
+            var queryString = "Value%5B0%5D=10&Value%5B1%5D=20&Value%5B2%5D=30".ToQueryString();
+
+            var untypedResult = Mapper.Unflatten(queryString).To(typeof(PublicField<Collection<string>>));
+
+            var result = (untypedResult as PublicField<Collection<string>>).ShouldNotBeNull();
+            
+            result.Value.ShouldNotBeNull();
+            result.Value.Count.ShouldBe(3);
+            result.Value[0].ShouldBe("10");
+            result.Value[1].ShouldBe("20");
+            result.Value[2].ShouldBe("30");
         }
     }
 }
