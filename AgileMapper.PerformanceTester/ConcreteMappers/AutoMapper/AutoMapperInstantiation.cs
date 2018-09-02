fed4305f69e198b2eab5bb19a76c23fc02dda002
Steve Wilkes <steve@agileobjects.co.uk>
2018-09-02 17:16:19 +0100

    Adding instance mapper instantiation perf tests

---
+++ AgileMapper.PerformanceTester/ConcreteMappers/AutoMapper/AutoMapperInstantiation.cs
@@ -0,0 +1,10 @@
+﻿namespace AgileObjects.AgileMapper.PerformanceTester.ConcreteMappers.AutoMapper
+{
+    using AbstractMappers;
+    using global::AutoMapper;
+
+    internal class AutoMapperInstantiation : MapperInstantiationBase
+    {
+        protected override object CreateMapperInstance() => new Mapper(new MapperConfiguration(cfg => { }));
+    }
+}
---
+++ AgileMapper.PerformanceTester/ConcreteMappers/Mapster/MapsterMapperInstantiation.cs
@@ -0,0 +1,10 @@
+﻿namespace AgileObjects.AgileMapper.PerformanceTester.ConcreteMappers.Mapster
+{
+    using AbstractMappers;
+    using global::Mapster;
+
+    internal class MapsterMapperInstantiation : MapperInstantiationBase
+    {
+        protected override object CreateMapperInstance() => new Adapter(new TypeAdapterConfig());
+    }
+}
