6e47c6b2a24fdcedb915bbe18d789a0d305b2745
Steve Wilkes <steve@agileobjects.co.uk>
2018-09-16 10:46:33 +0100

    Updating to v0.28

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.27.0")]
-[assembly: AssemblyFileVersion("0.27.0")]
+[assembly: AssemblyVersion("0.28.0")]
+[assembly: AssemblyFileVersion("0.28.0")]
