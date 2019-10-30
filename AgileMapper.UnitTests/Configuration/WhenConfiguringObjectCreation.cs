c3606a3088b25429ddb4d25e300b2f17d6ca0bfd
Steve Wilkes <steve@agileobjects.co.uk>
2019-10-30 08:33:53 +0000

    Splitting out incorrect object factory configuration tests

--- AgileMapper.UnitTests/Configuration/WhenConfiguringObjectCreation.cs
+++ AgileMapper.UnitTests/Configuration/WhenConfiguringObjectCreation.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
-    using AgileMapper.Configuration;
     using AgileMapper.Members;
     using Common;
     using TestClasses;
@@ -493,95 +492,6 @@
             mappingEx.InnerException.InnerException.ShouldNotBeNull();
             // ReSharper disable once PossibleNullReferenceException
             mappingEx.InnerException.InnerException.Message.ShouldBe("Can't make an address, sorry");
-        }
-
-        [Fact]
-        public void ShouldErrorIfSingleParameterObjectFactorySpecifiedWithInvalidParameter()
-        {
-            Should.Throw<MappingConfigurationException>(() =>
-            {
-                using (var mapper = Mapper.CreateNew())
-                {
-                    Func<DateTime, Address> addressFactory = dt => new Address();
-
-                    mapper
-                        .WhenMapping
-                        .InstancesOf<Address>()
-                        .CreateUsing(addressFactory);
-                }
-            });
-        }
-
-        [Fact]
-        public void ShouldErrorIfTwoParameterObjectFactorySpecifiedWithInvalidParameters()
-        {
-            Should.Throw<MappingConfigurationException>(() =>
-            {
-                using (var mapper = Mapper.CreateNew())
-                {
-                    Func<int, string, Address> addressFactory = (i, str) => new Address();
-
-                    mapper
-                        .WhenMapping
-                        .InstancesOf<Address>()
-                        .CreateUsing(addressFactory);
-                }
-            });
-        }
-
-        [Fact]
-        public void ShouldErrorIfFourParameterObjectFactorySpecified()
-        {
-            Should.Throw<MappingConfigurationException>(() =>
-            {
-                using (var mapper = Mapper.CreateNew())
-                {
-                    Func<object, object, int?, Address, Address> addressFactory = (i, str, dt, ts) => new Address();
-
-                    mapper
-                        .WhenMapping
-                        .InstancesOf<Address>()
-                        .CreateUsing(addressFactory);
-                }
-            });
-        }
-
-        [Fact]
-        public void ShouldErrorIfConflictingFactoryConfigured()
-        {
-            var factoryEx = Should.Throw<MappingConfigurationException>(() =>
-            {
-                using (var mapper = Mapper.CreateNew())
-                {
-                    mapper.WhenMapping
-                        .InstancesOf<Address>()
-                        .CreateUsing(ctx => new Address { Line1 = "Hello!" });
-
-                    mapper.WhenMapping
-                        .InstancesOf<Address>()
-                        .CreateUsing(ctx => new Address { Line1 = "Hello!" });
-                }
-            });
-
-            factoryEx.Message.ShouldContain("has already been configured");
-        }
-
-        // See https://github.com/agileobjects/AgileMapper/issues/114
-        [Fact]
-        public void ShouldErrorIfPrimitiveTargetTypeSpecified()
-        {
-            var factoryEx = Should.Throw<MappingConfigurationException>(() =>
-            {
-                using (var mapper = Mapper.CreateNew())
-                {
-                    mapper.WhenMapping
-                        .From<string>()
-                        .To<int>()
-                        .CreateInstancesUsing(ctx => 123);
-                }
-            });
-
-            factoryEx.Message.ShouldContain("primitive type 'int'");
         }
 
         #region Helper Classes
