3949c7add5c7d909917853edb55ce2250a31e578
Steve Wilkes <steve@agileobjects.co.uk>
2019-11-02 11:25:32 +0000

    TimeSpan mapping test coverage

---
+++ AgileMapper.UnitTests/SimpleTypeConversion/WhenConvertingToTimeSpans.cs
@@ -0,0 +1,51 @@
+﻿namespace AgileObjects.AgileMapper.UnitTests.SimpleTypeConversion
+{
+    using System;
+    using Common;
+    using TestClasses;
+#if !NET35
+    using Xunit;
+#else
+    using Fact = NUnit.Framework.TestAttribute;
+
+    [NUnit.Framework.TestFixture]
+#endif
+    public class WhenConvertingToTimeSpans
+    {
+        [Fact]
+        public void ShouldMapANullableTimeSpanToATimeSpan()
+        {
+            var source = new PublicProperty<TimeSpan?> { Value = TimeSpan.FromHours(1) };
+            var result = Mapper.Map(source).ToANew<PublicProperty<TimeSpan>>();
+
+            result.Value.ShouldBe(TimeSpan.FromHours(1));
+        }
+
+        [Fact]
+        public void ShouldMapAYearMonthDayStringToATimeSpan()
+        {
+            var source = new PublicProperty<string> { Value = "01:00:00" };
+            var result = Mapper.Map(source).ToANew<PublicProperty<TimeSpan>>();
+
+            result.Value.ShouldBe(TimeSpan.FromHours(01));
+        }
+
+        [Fact]
+        public void ShouldMapAnUnparseableStringToANullableTimeSpan()
+        {
+            var source = new PublicProperty<string> { Value = "OH OH OH" };
+            var result = Mapper.Map(source).ToANew<PublicProperty<TimeSpan?>>();
+
+            result.Value.ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldMapANullObjectStringToATimeSpan()
+        {
+            var source = new PublicProperty<object> { Value = default(string) };
+            var result = Mapper.Map(source).ToANew<PublicProperty<TimeSpan>>();
+
+            result.Value.ShouldBeDefault();
+        }
+    }
+}
