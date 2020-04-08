f3aa1d4e76cab05768713edd7387e75fda76f92e
Steve Wilkes <steve@agileobjects.co.uk>
2020-04-08 22:29:39 +0100

    Converting remaining projects to .NET Core project format, removing common AssemblyInfo files

--- AgileMapper.UnitTests.NonParallel/Properties/AssemblyInfo.cs
+++ AgileMapper.UnitTests.NonParallel/Properties/AssemblyInfo.cs
@@ -1,6 +1,3 @@
-﻿using System.Runtime.InteropServices;
-using Xunit;
-
-[assembly: ComVisible(false)]
+﻿using Xunit;
 
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
--- AgileMapper.UnitTests.Orms.Ef5/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("AgileObjects.AgileMapper.UnitTests.Ef6")]
-[assembly: AssemblyDescription("AgileObjects.AgileMapper.UnitTests.Ef6")]
-
-[assembly: ComVisible(false)]
