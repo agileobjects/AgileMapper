9b6475d94fceeb48e93f3742cfaa445a0566543d
Steve Wilkes <steve@agileobjects.co.uk>
2020-04-19 19:14:22 +0100

    Moving map methods back into interface to fix test overload resolution?!

--- AgileMapper/Api/Configuration/IRootMappingConfigurator.cs
+++ AgileMapper/Api/Configuration/IRootMappingConfigurator.cs
@@ -10,7 +10,7 @@ namespace AgileObjects.AgileMapper.Api.Configuration
     /// </summary>
     /// <typeparam name="TSource">The source type to which the configuration should apply.</typeparam>
     /// <typeparam name="TTarget">The target type to which the configuration should apply.</typeparam>
-    public interface IRootMappingConfigurator<TSource, TTarget> : IMapSourceConfigurator<TSource, TTarget>
+    public interface IRootMappingConfigurator<TSource, TTarget>
     {
         /// <summary>
         /// Use the given <paramref name="factory"/> expression to create instances of the target
@@ -237,6 +237,48 @@ namespace AgileObjects.AgileMapper.Api.Configuration
         /// </returns>
         IMappingConfigContinuation<TSource, TTarget> IgnoreTargetMembersWhere(
             Expression<Func<TargetMemberSelector, bool>> memberFilter);
+
+        /// <summary>
+        /// Configure a custom data source for a particular target member when mapping from and to
+        /// the source and target types being configured. The factory expression is passed a context
+        /// object containing the current mapping's source and target objects.
+        /// </summary>
+        /// <typeparam name="TSourceValue">The type of the custom value being configured.</typeparam>
+        /// <param name="valueFactoryExpression">The expression to map to the configured target member.</param>
+        /// <returns>
+        /// An ICustomDataSourceMappingConfigContinuation with which to control the reverse configuration,
+        /// or further configure mappings from and to the source and target type being configured.
+        /// </returns>
+        ICustomDataSourceTargetMemberSpecifier<TSource, TTarget> Map<TSourceValue>(
+            Expression<Func<IMappingData<TSource, TTarget>, TSourceValue>> valueFactoryExpression);
+
+        /// <summary>
+        /// Configure a custom data source for a particular target member when mapping from and to
+        /// the source and target types being configured. The factory expression is passed the current
+        /// mapping's source and target objects.
+        /// </summary>
+        /// <typeparam name="TSourceValue">The type of the custom value being configured.</typeparam>
+        /// <param name="valueFactoryExpression">The expression to map to the configured target member.</param>
+        /// <returns>
+        /// An ICustomDataSourceTargetMemberSpecifier with which to specify the target member to
+        /// which the custom value should be applied.
+        /// </returns>
+        ICustomDataSourceTargetMemberSpecifier<TSource, TTarget> Map<TSourceValue>(
+            Expression<Func<TSource, TTarget, TSourceValue>> valueFactoryExpression);
+
+        /// <summary>
+        /// Configure a custom data source for a particular target member when mapping from and to
+        /// the source and target types being configured. The factory expression is passed the current
+        /// mapping's source and target objects and the current element index, if applicable.
+        /// </summary>
+        /// <typeparam name="TSourceValue">The type of the custom value being configured.</typeparam>
+        /// <param name="valueFactoryExpression">The expression to map to the configured target member.</param>
+        /// <returns>
+        /// An ICustomDataSourceTargetMemberSpecifier with which to specify the target member to
+        /// which the custom value should be applied.
+        /// </returns>
+        ICustomDataSourceTargetMemberSpecifier<TSource, TTarget> Map<TSourceValue>(
+            Expression<Func<TSource, TTarget, int?, TSourceValue>> valueFactoryExpression);
 
         /// <summary>
         /// Configure a Func object to be mapped to a target member of the same Func signature.
