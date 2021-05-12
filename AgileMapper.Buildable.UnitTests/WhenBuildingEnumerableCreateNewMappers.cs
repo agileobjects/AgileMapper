6589b899bd1ba30b94c989be16b882b4e8897015
Steve Wilkes <steve@agileobjects.co.uk>
2021-05-12 18:18:31 +0100

    Test coverage for building a simple create new enumerable mapper

---
+++ AgileMapper.Buildable.UnitTests/WhenBuildingEnumerableCreateNewMappers.cs
@@ -0,0 +1,44 @@
+﻿namespace AgileObjects.AgileMapper.Buildable.UnitTests
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using AgileMapper.UnitTests.Common;
+    using Plans;
+    using Xunit;
+
+    public class WhenBuildingEnumerableCreateNewMappers
+    {
+        [Fact]
+        public void ShouldBuildSimpleTypeListToCollectionMapper()
+        {
+            using (var mapper = Mapper.CreateNew())
+            {
+                mapper.GetPlanFor<List<string>>().ToANew<Collection<byte?>>(new MappingPlanSettings
+                {
+                    LazyCompile = true
+                });
+
+                var sourceCodeExpressions = mapper.BuildSourceCode();
+
+                var staticMapperClass = sourceCodeExpressions
+                    .ShouldCompileAStaticMapperClass();
+
+                var staticMapMethod = staticMapperClass
+                    .GetMapMethods()
+                    .ShouldHaveSingleItem();
+
+                var source = new List<string> { "3", "2", "1", "12345" };
+
+                var executor = staticMapMethod
+                    .ShouldCreateMappingExecutor(source);
+
+                var result = executor
+                    .ShouldHaveACreateNewMethod()
+                    .ShouldExecuteACreateNewMapping<Collection<byte?>>(executor);
+
+                result.ShouldNotBeNull();
+                result.ShouldBe<byte?>(3, 2, 1, null);
+            }
+        }
+    }
+}
