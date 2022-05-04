c2afde23372abfb579cd5829e7953e4181c276f6
Steve Wilkes <steve@agileobjects.co.uk>
2022-05-04 20:58:06 +0100

    Ensuring try/catch Exception variables have unique names

--- AgileMapper/Members/Extensions/MemberExpressionExtensions.cs
+++ AgileMapper/Members/Extensions/MemberExpressionExtensions.cs
@@ -1,45 +1,69 @@
-namespace AgileObjects.AgileMapper.Members.Extensions
-{
-    using System;
+namespace AgileObjects.AgileMapper.Members.Extensions;
+
+using System;
 #if NET35
-    using Microsoft.Scripting.Ast;
+using Microsoft.Scripting.Ast;
 #else
-    using System.Linq.Expressions;
+using System.Linq.Expressions;
 #endif
+using AgileMapper.Extensions.Internal;
 
-    internal static class MemberExpressionExtensions
+internal static class MemberExpressionExtensions
+{
+    public static bool IsMappingContextCall(this MethodCallExpression methodCall)
+        => methodCall.Object == Constants.ExecutionContextParameter;
+
+    public static TryExpression WrapInTryCatch(this Expression mapping, IMemberMapperData mapperData)
     {
-        public static bool IsMappingContextCall(this MethodCallExpression methodCall)
-            => methodCall.Object == Constants.ExecutionContextParameter;
+        var configuredCallback = mapperData
+            .MapperContext.UserConfigurations
+            .GetExceptionCallbackOrNull(mapperData);
 
-        public static TryExpression WrapInTryCatch(this Expression mapping, IMemberMapperData mapperData)
+        var exceptionVariable = typeof(Exception)
+            .GetOrCreateParameter(mapperData.GetExceptionName());
+
+        if (configuredCallback == null)
         {
-            var configuredCallback = mapperData.MapperContext.UserConfigurations.GetExceptionCallbackOrNull(mapperData);
-            var exceptionVariable = Parameters.Create<Exception>("ex");
+            var catchBody = Expression.Throw(
+                MappingException.GetFactoryMethodCall(mapperData, exceptionVariable),
+                mapping.Type);
 
-            if (configuredCallback == null)
-            {
-                var catchBody = Expression.Throw(
-                    MappingException.GetFactoryMethodCall(mapperData, exceptionVariable),
-                    mapping.Type);
-
-                return CreateTryCatch(mapping, exceptionVariable, catchBody);
-            }
-
-            var configuredCatchBody = configuredCallback
-                .ToCatchBody(exceptionVariable, mapping.Type, mapperData);
-
-            return CreateTryCatch(mapping, exceptionVariable, configuredCatchBody);
+            return CreateTryCatch(mapping, exceptionVariable, catchBody);
         }
 
-        private static TryExpression CreateTryCatch(
-            Expression mappingBlock,
-            ParameterExpression exceptionVariable,
-            Expression catchBody)
-        {
-            var catchBlock = Expression.Catch(exceptionVariable, catchBody);
+        var configuredCatchBody = configuredCallback
+            .ToCatchBody(exceptionVariable, mapping.Type, mapperData);
 
-            return Expression.TryCatch(mappingBlock, catchBlock);
+        return CreateTryCatch(
+            mapping,
+            exceptionVariable,
+            configuredCatchBody);
+    }
+
+    private static string GetExceptionName(this IMemberMapperData mapperData)
+    {
+        var suffix = mapperData.TargetMember.Depth;
+
+        if (mapperData.Context.IsForDerivedType)
+        {
+            ++suffix;
         }
+
+        if (suffix == 1)
+        {
+            return "ex";
+        }
+
+        return "ex" + suffix;
+    }
+
+    private static TryExpression CreateTryCatch(
+        Expression mappingBlock,
+        ParameterExpression exceptionVariable,
+        Expression catchBody)
+    {
+        var catchBlock = Expression.Catch(exceptionVariable, catchBody);
+
+        return Expression.TryCatch(mappingBlock, catchBlock);
     }
 }
--- AgileMapper/ObjectPopulation/ComplexTypes/TargetObjectResolutionFactory.cs
+++ AgileMapper/ObjectPopulation/ComplexTypes/TargetObjectResolutionFactory.cs
@@ -1,134 +1,133 @@
-namespace AgileObjects.AgileMapper.ObjectPopulation.ComplexTypes
-{
-    using System;
-    using System.Collections.Generic;
+namespace AgileObjects.AgileMapper.ObjectPopulation.ComplexTypes;
+
+using System;
+using System.Collections.Generic;
 #if NET35
-    using Microsoft.Scripting.Ast;
+using Microsoft.Scripting.Ast;
 #else
-    using System.Linq.Expressions;
+using System.Linq.Expressions;
 #endif
-    using Extensions.Internal;
-    using Members;
-    using NetStandardPolyfills;
+using Extensions.Internal;
+using Members;
+using NetStandardPolyfills;
 
-    internal static class TargetObjectResolutionFactory
+internal static class TargetObjectResolutionFactory
+{
+    public static Expression GetObjectResolution(
+        Expression construction,
+        IObjectMappingData mappingData,
+        bool assignTargetObject = false)
     {
-        public static Expression GetObjectResolution(
-            Expression construction,
-            IObjectMappingData mappingData,
-            bool assignTargetObject = false)
+        return GetObjectResolution(
+            (_, _) => construction,
+            mappingData,
+            null,
+            assignCreatedObject: false,
+            assignTargetObject);
+    }
+
+    public static Expression GetObjectResolution(
+        Func<IObjectMappingData, IList<Expression>, Expression> constructionFactory,
+        IObjectMappingData mappingData,
+        IList<Expression> memberPopulations,
+        bool assignCreatedObject,
+        bool assignTargetObject = false)
+    {
+        var mapperData = mappingData.MapperData;
+
+        if (mapperData.TargetMember.IsReadOnly || mapperData.TargetIsDefinitelyPopulated())
         {
-            return GetObjectResolution(
-                (_, _) => construction,
-                mappingData,
-                null,
-                false,
-                assignTargetObject);
+            return mapperData.TargetObject;
         }
 
-        public static Expression GetObjectResolution(
-            Func<IObjectMappingData, IList<Expression>, Expression> constructionFactory,
-            IObjectMappingData mappingData,
-            IList<Expression> memberPopulations,
-            bool assignCreatedObject = false,
-            bool assignTargetObject = false)
+        var objectValue = constructionFactory.Invoke(mappingData, memberPopulations);
+
+        if (objectValue == null)
         {
-            var mapperData = mappingData.MapperData;
-
-            if (mapperData.TargetMember.IsReadOnly || mapperData.TargetIsDefinitelyPopulated())
+            if (MarkUnconstructableMemberAsReadOnly(mappingData))
             {
-                return mapperData.TargetObject;
+                mapperData.TargetMember.IsReadOnly = true;
             }
 
-            var objectValue = constructionFactory.Invoke(mappingData, memberPopulations);
-
-            if (objectValue == null)
-            {
-                if (MarkUnconstructableMemberAsReadOnly(mappingData))
-                {
-                    mapperData.TargetMember.IsReadOnly = true;
-                }
-
-                // Use the existing target object if it might have a value and
-                // the mapper can't create an instance:
-                return mapperData.TargetCouldBePopulated()
-                    ? mapperData.TargetObject
-                    : mapperData.GetTargetMemberDefault();
-            }
-
-            if (UseNullFallbackValue(mapperData, objectValue, memberPopulations))
-            {
-                objectValue = mapperData.GetTargetMemberDefault();
-                mapperData.Context.NeedsMappingData = false;
-            }
-            else
-            {
-                if (assignCreatedObject)
-                {
-                    // TODO mapperData.Context.UsesMappingDataObjectAsParameter = true;
-                    objectValue = mapperData.CreatedObject.AssignTo(objectValue);
-                }
-
-                if (assignTargetObject || mapperData.Context.NeedsMappingData)
-                {
-                    objectValue = mapperData.TargetObject.AssignTo(objectValue);
-                }
-            }
-
-            objectValue = AddExistingTargetCheckIfAppropriate(objectValue, mapperData);
-            return objectValue;
+            // Use the existing target object if it might have a value and
+            // the mapper can't create an instance:
+            return mapperData.TargetCouldBePopulated()
+                ? mapperData.TargetObject
+                : mapperData.GetTargetMemberDefault();
         }
 
-        private static bool MarkUnconstructableMemberAsReadOnly(IObjectMappingData mappingData)
+        if (UseNullFallbackValue(mapperData, objectValue, memberPopulations))
         {
-            if (mappingData.HasSameTypedConfiguredDataSource())
+            objectValue = mapperData.GetTargetMemberDefault();
+            mapperData.Context.NeedsMappingData = false;
+        }
+        else
+        {
+            if (assignCreatedObject)
             {
-                // Configured data source for an otherwise-unconstructable complex type:
-                return false;
+                // TODO mapperData.Context.UsesMappingDataObjectAsParameter = true;
+                objectValue = mapperData.CreatedObject.AssignTo(objectValue);
             }
+
+            if (assignTargetObject || mapperData.Context.NeedsMappingData)
+            {
+                objectValue = mapperData.TargetObject.AssignTo(objectValue);
+            }
+        }
+
+        objectValue = AddExistingTargetCheckIfAppropriate(objectValue, mapperData);
+        return objectValue;
+    }
+
+    private static bool MarkUnconstructableMemberAsReadOnly(IObjectMappingData mappingData)
+    {
+        if (mappingData.HasSameTypedConfiguredDataSource())
+        {
+            // Configured data source for an otherwise-unconstructable complex type:
+            return false;
+        }
                 
-            // Don't set an non-readonly abstract member to readonly as we'll try to map
-            // it from derived types:
-            return !mappingData.MapperData.TargetMember.Type.IsAbstract();
-        }
+        // Don't set an non-readonly abstract member to readonly as we'll try to map
+        // it from derived types:
+        return !mappingData.MapperData.TargetMember.Type.IsAbstract();
+    }
 
-        private static bool UseNullFallbackValue(
-            IQualifiedMemberContext context,
-            Expression objectConstruction,
-            IList<Expression> memberPopulations)
+    private static bool UseNullFallbackValue(
+        IQualifiedMemberContext context,
+        Expression objectConstruction,
+        IList<Expression> memberPopulations)
+    {
+        if (memberPopulations == null)
         {
-            if (memberPopulations == null)
-            {
-                return false;
-            }
-
-            if ((objectConstruction.NodeType != ExpressionType.New) ||
-                 MemberPopulationsExist(memberPopulations) ||
-                 context.SourceMember.Matches(context.TargetMember))
-            {
-                return false;
-            }
-
-            var objectNewing = (NewExpression)objectConstruction;
-
-            return objectNewing.Arguments.None();
+            return false;
         }
 
-        private static bool MemberPopulationsExist(IList<Expression> populationsAndCallbacks)
-            => populationsAndCallbacks.Any(population => population.NodeType != ExpressionType.Constant);
-
-        private static Expression AddExistingTargetCheckIfAppropriate(Expression value, IMemberMapperData mapperData)
+        if ((objectConstruction.NodeType != ExpressionType.New) ||
+            MemberPopulationsExist(memberPopulations) ||
+            context.SourceMember.Matches(context.TargetMember))
         {
-            if ((value.NodeType == ExpressionType.Default) ||
-                mapperData.RuleSet.Settings.UseSingleRootMappingExpression ||
-                !mapperData.TargetMember.IsReadable ||
-                mapperData.TargetMemberIsUserStruct() ||
-                mapperData.TargetIsDefinitelyUnpopulated())
-            {
-                return value;
-            }
-
-            return Expression.Coalesce(mapperData.TargetObject, value);
+            return false;
         }
+
+        var objectNewing = (NewExpression)objectConstruction;
+
+        return objectNewing.Arguments.None();
+    }
+
+    private static bool MemberPopulationsExist(IList<Expression> populationsAndCallbacks)
+        => populationsAndCallbacks.Any(population => population.NodeType != ExpressionType.Constant);
+
+    private static Expression AddExistingTargetCheckIfAppropriate(Expression value, IMemberMapperData mapperData)
+    {
+        if ((value.NodeType == ExpressionType.Default) ||
+            mapperData.RuleSet.Settings.UseSingleRootMappingExpression ||
+            !mapperData.TargetMember.IsReadable ||
+            mapperData.TargetMemberIsUserStruct() ||
+            mapperData.TargetIsDefinitelyUnpopulated())
+        {
+            return value;
+        }
+
+        return Expression.Coalesce(mapperData.TargetObject, value);
     }
 }
